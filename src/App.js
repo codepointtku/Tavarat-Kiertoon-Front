6602978e4696b189d5e22850da404a187ee125a4
ErkkoV <erkko.vallivaara@gmail.com>
2022-12-14 10:09:45 +0200

    changed path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             element: <Base />,
             children: [
                 {
-                    path: '/',
+                    path: '/tavaratkiertoon',
                     element: <TavaratKiertoon />,
                 },
             ],
