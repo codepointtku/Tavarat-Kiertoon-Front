3b5804fb8598a263cb168fa18f51b3f73cebec51
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-11 12:58:11 +0200

    added admin pathing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Base from './MainComponents/Base';
 import TavaratKiertoon from './MainComponents/TavaratKiertoon';
 import StorageView from './MainComponents/StorageView';
+import AdminView from './MainComponents/AdminView';
 
 function App() {
     const router = createBrowserRouter([
@@ -17,6 +18,10 @@ function App() {
                 {
                     path: '/varasto',
                     element: <StorageView />,
+                },
+                {
+                    path: '/admin',
+                    element: <AdminView />,
                 },
             ],
         },
