afbb2f6cde9382655160db9b614c01e6dbc6a46f
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-11 12:52:22 +0200

    added redirect if user is not authed

--- src/MainComponents/StorageView.js
+++ src/MainComponents/StorageView.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { redirect } from 'react-router-dom';
+
 function StorageView() {
+    // replace this with authcontext later
+    const auth = false;
+
+    useEffect(() => {
+        if (!auth) {
+            return redirect('/tavaratkiertoon');
+        }
+        return null;
+    }, [auth]);
+
     return <div>ToniPal Kahville!</div>;
 }
 
