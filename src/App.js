cdd29eaf361dbdf8d2ae4a51e777d7d8f4314cbe
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-11 12:43:15 +0200

    working base component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { createBrowserRouter, RouterProvider, redirect } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Base from './MainComponents/Base';
 import TavaratKiertoon from './MainComponents/TavaratKiertoon';
 import StorageView from './MainComponents/StorageView';
 
 function App() {
-    // replace this with userContext
-    const tempContext = true;
-
     const router = createBrowserRouter([
         {
             path: '/',
@@ -20,11 +17,6 @@ function App() {
                 {
                     path: '/varasto',
                     element: <StorageView />,
-                    loader: () => {
-                        if (!tempContext) {
-                            throw redirect('/tavaratkiertoon');
-                        }
-                    },
                 },
             ],
         },
