4aefc6584f14654f482beb30d1cb43a871ce7b0a
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2023-01-11 13:50:34 +0200

    Added Playwright and an example test

---
+++ tests/example.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+});
+
+test.describe('/ page', () => {
+    test('should have Header text', async ({ page }) => {
+        await expect(page.getByText('Header')).toBeVisible();
+    });
+});
+
+test.describe('tavaratkiertoon page', () => {
+    test('should have Tavarat Kiertoon text', async ({ page }) => {
+        await page.goto('/tavaratkiertoon');
+        await expect(page.getByText('Tavarat Kiertoon')).toBeVisible();
+    });
+});
