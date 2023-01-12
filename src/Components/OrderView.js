799639df4c029dc95d28b200944185d740ca6a5a
ExcavatorZ <excavatorz@hotmail.fi>
2023-01-12 09:52:02 +0200

    Added imports and test data.

--- src/Components/OrderView.js
+++ src/Components/OrderView.js
@@ -1,3 +1,15 @@
+import { Table, TableBody, TableContainer, TableHead, TableCell, TableRow, Paper } from '@mui/material';
+
+function orderData(name, category, date) {
+    return { name, category, date };
+}
+
+// Test data
+const rows = [
+    orderData('Tuoli', 'furniture', '12.1.2023'),
+    orderData('Tonipal kahville', 'inspirational quote', '12.1.2023'),
+];
+
 function OrderView() {
     return <div>Tilausnäkymä</div>;
 }
