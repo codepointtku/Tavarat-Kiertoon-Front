60f5b543bedb726f7b3089da5ebe943afb7bded4
J. Waananen <j.waananen@protonmail.com>
2023-01-12 15:52:04 +0200

    default view template for future development

--- src/MainComponents/DefaultView.js
+++ src/MainComponents/DefaultView.js
@@ -1,5 +1,14 @@
+import '../Styles/defaultview.css';
+
 function DefaultView() {
-    return <div>Tavarat Kiertoon</div>;
+    return (
+        <>
+            <h1>Tavarat Kiertoon</h1>
+            <div className="komponentti">tähän komponentti, ei divejä</div>
+            <div className="komponentti">tähän myös</div>
+            <div>ja niin edelleen</div>
+        </>
+    );
 }
 
 export default DefaultView;
