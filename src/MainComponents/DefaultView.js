178c93dbfdaee3cee3e909f839ef4f1f54fd4b99
J. Waananen <j.waananen@protonmail.com>
2023-01-13 07:40:22 +0200

    cleaned test boilerplate

--- src/MainComponents/DefaultView.js
+++ src/MainComponents/DefaultView.js
@@ -4,9 +4,7 @@ function DefaultView() {
     return (
         <>
             <h1>Tavarat Kiertoon</h1>
-            <div className="komponentti">tähän komponentti, ei divejä</div>
-            <div className="komponentti">tähän myös</div>
-            <div>ja niin edelleen</div>
+            <div>tähän komponentteja, ei divejä yms</div>
         </>
     );
 }
