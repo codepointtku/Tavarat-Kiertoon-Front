62ba99c0ff96aafa4659df66bc586adc2d377c61
tojoju <35487991+tojoju@users.noreply.github.com>
2023-01-18 12:48:10 +0200

    Add Auth Context
    Adding auth context and TestButtons to test the feature (including links to pages). TestButtons need to be deleted once proper implementation is done.

--- src/Layouts/Components/Header.js
+++ src/Layouts/Components/Header.js
@@ -1,9 +1,84 @@
 import { Button } from '@mui/material';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../Context/AuthContext';
 
 function Header() {
+    // JTo: Commented while TestButtons are in use.
+    // const { auth, setAuth } = useContext(AuthContext);
+
     return (
         <div>
             <Button>Header Button</Button>
+            <TestButtons />
+        </div>
+    );
+}
+
+/**
+ * JTo: This is for TESTING page navigation and Auth Context.
+ * Remove this when real Login and Navigation codes are done
+ * @returns
+ */
+function TestButtons() {
+    const { auth, setAuth } = useContext(AuthContext);
+
+    // admin, storage, user
+    const loginUserButtonHandler = () => {
+        const newAuth = { user: !auth.user, storage: false, admin: false };
+        setAuth(newAuth);
+    };
+    const loginStorageButtonHandler = () => {
+        const newAuth = { user: false, storage: !auth.storage, admin: false };
+        setAuth(newAuth);
+    };
+    const loginAdminButtonHandler = () => {
+        const newAuth = { user: false, storage: false, admin: !auth.admin };
+        setAuth(newAuth);
+    };
+
+    return (
+        <div>
+            <hr />
+            <div style={{ marginBottom: '5px' }}>
+                <b>Links to pages: </b>
+                <Link to="/">Home</Link> <Link to="/varasto">Varasto</Link>{' '}
+                <Link to="/varasto/tilaus">Varasto/Tilaus</Link>{' '}
+                <Link to="/varasto/koodinlukija">Varasto/Koodinlukija</Link>
+                <br />
+                <Link to="/admin">Admin</Link> <Link to="/admin/user">Admin/Käyttäjä</Link>{' '}
+                <Link to="/admin/varastot">Admin/Varastot</Link>{' '}
+                <Link to="/admin/varastot/varasto">Admin/Varastot/Varasto</Link>{' '}
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <b>Logins:</b>
+                <button
+                    type="button"
+                    onClick={loginUserButtonHandler}
+                    style={{
+                        marginLeft: '5px',
+                        marginRight: '5px',
+                        background: auth.user ? 'lightgreen' : 'lightsalmon',
+                    }}
+                >
+                    Käyttäjä: {auth.user ? 'Log Out' : 'Log In'}
+                </button>
+                <button
+                    type="button"
+                    onClick={loginStorageButtonHandler}
+                    style={{ marginRight: '5px', background: auth.storage ? 'lightgreen' : 'lightsalmon' }}
+                >
+                    Varasto: {auth.storage ? 'Log Out' : 'Log In'}
+                </button>
+                <button
+                    type="button"
+                    onClick={loginAdminButtonHandler}
+                    style={{ background: auth.admin ? 'lightgreen' : 'lightsalmon' }}
+                >
+                    Admin: {auth.admin ? 'Log Out' : 'Log In'}
+                </button>
+            </div>
+            <hr />
         </div>
     );
 }
--- src/Routes/DefaultView.js
+++ src/Routes/DefaultView.js
@@ -1,6 +1,11 @@
 import { Button } from '@mui/material';
+import { useContext } from 'react';
+import AuthContext from '../Context/AuthContext';
 
 function DefaultView() {
+    const { auth } = useContext(AuthContext);
+    console.log('Auth', auth); // JTo: console log to get rid of ESLint error. Remove once context is taken into use
+
     return (
         <>
             <h1>Tavarat Kiertoon</h1>
