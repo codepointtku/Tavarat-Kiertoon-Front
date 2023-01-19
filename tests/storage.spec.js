cdc80a6b9a7e9be4713c40a543f1d217f0a77e28
ExcavatorZ <excavatorz@hotmail.fi>
2023-01-19 14:56:21 +0200

    Configured and added tests for current layout.

--- tests/storage.spec.js
+++ tests/storage.spec.js
@@ -2,23 +2,28 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    await page.click('//*[@id="root"]/div[1]/div/div[2]/button[2]');
+    await page.click('//*[@id="root"]/div[1]/div/div[1]/a[2]');
 });
 
 test.describe('storage view', () => {
     test('initial', async ({ page }) => {
-        await page.goto('/varasto');
         await expect(page.getByText('Tilaus (ID)')).toBeVisible();
     });
     test('next page', async ({ page }) => {
-        await page.goto('/varasto');
         await page.click('button[aria-label="next page"]');
         await expect(page.getByText('6–10 of 12')).toBeVisible();
     });
     test('last page', async ({ page }) => {
-        await page.goto('/varasto');
         await page.click('button[aria-label="last page"]');
-        await page.locator('tr').first().waitFor();
-        // console.log(await page.locator('tr').count());
-        await ((await expect(page.locator('tr').count())) === 5);
+        await expect(page.getByText('11–12 of 12')).toBeVisible();
+    });
+    test('10 rows per page', async ({ page }) => {
+        await page.locator('select[aria-label="rows per page"]').selectOption({ label: '10' });
+        await expect(page.getByText('1–10 of 12')).toBeVisible();
+    });
+    test('all rows', async ({ page }) => {
+        await page.locator('select[aria-label="rows per page"]').selectOption({ label: 'All' });
+        await expect(page.getByText('1–12 of 12')).toBeVisible();
     });
 });
