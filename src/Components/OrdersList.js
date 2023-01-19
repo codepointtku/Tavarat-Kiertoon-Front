330643f646cca5ef9d3d53c17f2c06ab6e52cf27
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-19 08:22:04 +0200

    working useState style searchParams custom hook

--- src/Components/OrdersList.js
+++ src/Components/OrdersList.js
@@ -1,7 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 import OrderListTable from './DataTable';
 
 function OrdersList() {
-    return <OrderListTable />;
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const useCheckSearchParams = (params, check) => {
+        const [paramState, setParamState] = useState();
+
+        useEffect(() => {
+            if (searchParams.get(check) === null) {
+                setSearchParams(params);
+            }
+        }, []);
+
+        useEffect(() => {
+            const newParams = {};
+            const keys = Array.from(searchParams.keys());
+
+            keys.forEach((key) => {
+                newParams[key] = searchParams.get(key);
+            });
+            setParamState(newParams);
+            console.log(newParams);
+        }, [searchParams]);
+
+        const paramSet = (values) => {
+            setSearchParams(values);
+        };
+
+        return [paramState, paramSet];
+    };
+
+    const [usedParams, setUsedParams] = useCheckSearchParams({ page: 1, rows: 5 }, 'page');
+
+    useEffect(() => {
+        console.log(usedParams);
+    }, [usedParams]);
+
+    return (
+        <>
+            <button
+                type="button"
+                onClick={() => {
+                    setUsedParams({ page: 3, rows: 25 });
+                }}
+            >
+                Test
+            </button>
+            {usedParams && <h1>{usedParams.page}</h1>}
+            <OrderListTable />
+        </>
+    );
 }
 
 export default OrdersList;
