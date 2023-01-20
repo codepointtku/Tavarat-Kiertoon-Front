506daaafbab8980ac4879a34bcad4e3625f17c4b
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-01-20 11:40:23 +0200

    Added basic material ui boilerplate

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,9 +1,32 @@
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Paper, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 function ProductDetails() {
     const { productId } = useParams();
 
-    return <div>ProductDetails id:{productId} - tuotteen yksityiskohtaiset tiedot</div>;
+    return (
+        <Box>
+            <Paper elevation={3}>
+                <Card sx={{ maxWidth: 1200 }}>
+                    <CardMedia component="img" alt="turku logo" height="300" image="turku_pysty_300ppi_black.png" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Product
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across
+                            all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>{' '}
+                <Typography> ProductDetails id:{productId} - tuotteen yksityiskohtaiset tiedot</Typography>
+            </Paper>
+        </Box>
+    );
 }
 
 export default ProductDetails;
