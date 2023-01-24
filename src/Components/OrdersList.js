3a581373be319ffa995742324540e35e8ae1064c
ErkkoV <102948460+ErkkoV@users.noreply.github.com>
2023-01-24 13:30:54 +0200

    working with component

--- src/Components/OrdersList.js
+++ src/Components/OrdersList.js
@@ -8,11 +8,10 @@ import useCustomSearchParams from '../Hooks/useCustomSearchParams';
 function OrdersList() {
     const [usedParams, setUsedParams] = useCustomSearchParams({ page: 0, rows: 5 });
     const data = useLoaderData();
-    console.log(data)
-
+    
     return (
         usedParams && (
-            <OrderListTable page={usedParams.page} rowsPerPage={usedParams.rows} setUsedParams={setUsedParams} />
+            <OrderListTable page={usedParams.page} rowsPerPage={usedParams.rows} setUsedParams={setUsedParams} rows={data} />
         )
     );
 }
