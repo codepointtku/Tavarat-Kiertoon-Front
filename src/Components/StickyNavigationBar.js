04c78a8a549a2f0d4faccf44a9407e3831ab89b9
J. Waananen <j.waananen@protonmail.com>
2023-01-24 15:57:50 +0200

    functionality, state handling

--- src/Components/StickyNavigationBar.js
+++ src/Components/StickyNavigationBar.js
@@ -1,11 +1,45 @@
+import { useState } from 'react';
+
 import { styled, alpha } from '@mui/material/styles';
 
-import { AppBar, Box, Toolbar, InputBase, IconButton, Stack } from '@mui/material';
-// import MenuIcon from '@mui/icons-material/Menu';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    InputBase,
+    IconButton,
+    Stack,
+    Badge,
+    Drawer as MuiDrawer,
+    List,
+    Divider,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    Container,
+    Button,
+} from '@mui/material';
+
+// imports for mock login/signup -form
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+
+// icons
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import CallIcon from '@mui/icons-material/Call';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+//
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -41,47 +75,206 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('sm')]: {
-            width: '12ch',
+            width: '14ch',
             '&:focus': {
-                width: '20ch',
+                width: '22ch',
             },
         },
     },
 }));
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: -8,
+        border: `0.1rem solid ${theme.palette.background.paper}`,
+        backgroundColor: `${theme.palette.primary.light}`,
+    },
+}));
+
+//
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+}));
+
+const drawerWidth = 340;
+
+function Drawer({ currentOpenDrawer, name, children }) {
+    return (
+        <MuiDrawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    zIndex: 1050,
+                    width: drawerWidth,
+                },
+            }}
+            variant="persistent"
+            anchor="right"
+            open={currentOpenDrawer === name}
+        >
+            <DrawerHeader />
+            <Divider />
+            {children}
+        </MuiDrawer>
+    );
+}
+
 function StickyNavigationBar() {
+    const [currentOpenDrawer, setCurrentOpenDrawer] = useState(null);
+
+    const drawerOpen = (drawer) => () => {
+        if (currentOpenDrawer === drawer) {
+            setCurrentOpenDrawer(null);
+        } else {
+            setCurrentOpenDrawer(drawer);
+        }
+    };
+
     return (
         <Box>
             <AppBar
                 sx={{
                     width: 'min-content',
+                    boxShadow: 0,
                 }}
             >
                 <Toolbar
                     sx={{
                         justifyContent: 'flex-end',
-                        // alignItems: 'stretch',
                     }}
                 >
                     <Stack direction="row" spacing={4}>
                         <Search>
                             <SearchIconWrapper>
-                                <SearchIcon />
+                                <SearchIcon sx={{ fontSize: 30, color: '#fff' }} />
                             </SearchIconWrapper>
                             <StyledInputBase placeholder="Etsi tuotteita…" inputProps={{ 'aria-label': 'search' }} />
                         </Search>
-                        <IconButton>
-                            <ShoppingCartOutlinedIcon sx={{ fontSize: 30, color: '#fff' }} />
+                        <IconButton onClick={drawerOpen('shoppingCart')}>
+                            <StyledBadge
+                                badgeContent={4}
+                                sx={{ color: 'primary.contrastText' }}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                            >
+                                <ShoppingCartOutlinedIcon sx={{ fontSize: 30, color: '#fff' }} />
+                            </StyledBadge>
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={drawerOpen('account')}>
                             <AccountCircleOutlinedIcon sx={{ fontSize: 30, color: '#fff' }} />
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={drawerOpen('contact')}>
                             <CallIcon sx={{ fontSize: 30, color: '#fff' }} />
                         </IconButton>
                     </Stack>
                 </Toolbar>
             </AppBar>
+            <Drawer currentOpenDrawer={currentOpenDrawer} name="shoppingCart">
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Drawer currentOpenDrawer={currentOpenDrawer} name="account">
+                <div>
+                    <Typography variant="body2" color="text.secondary" align="center">
+                        Sun accountti
+                    </Typography>
+
+                    <Container component="main" maxWidth="xs">
+                        <Box
+                            sx={{
+                                marginTop: 8,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Kirjaudu sisään
+                            </Typography>
+                            <Box component="form" noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" />}
+                                    label="Remember me"
+                                />
+                                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link variant="body2">Forgot password?</Link>
+                                    </Grid>
+                                    <Grid item>
+                                        <Link variant="body2">Don't have an account? Sign Up</Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Container>
+                </div>
+            </Drawer>
+            <Drawer currentOpenDrawer={currentOpenDrawer} name="contact">
+                <List>
+                    {['Testi', 'Moikka', 'Send email', 'Jeppistä'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
         </Box>
     );
 }
