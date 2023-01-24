496308586f2172fb4d0546ed4a7846eb77cef38e
ErkkoV <102948460+ErkkoV@users.noreply.github.com>
2023-01-24 12:39:13 +0200

    testing

--- src/Components/OrdersList.js
+++ src/Components/OrdersList.js
@@ -1,8 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
 import OrderListTable from './DataTable';
 import useCustomSearchParams from '../Hooks/useCustomSearchParams';
 
+
+
 function OrdersList() {
     const [usedParams, setUsedParams] = useCustomSearchParams({ page: 0, rows: 5 });
+    const data = useLoaderData();
+    console.log(data)
 
     return (
         usedParams && (
