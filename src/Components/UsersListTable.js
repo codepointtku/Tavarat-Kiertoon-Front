1ba2d139f82f02fa63a89445387a0b437a78a2cd
ExcavatorZ <taneli.murtola@turku.fi>
2023-01-26 10:07:15 +0200

    Added collapsible table.

--- src/Components/UsersListTable.js
+++ src/Components/UsersListTable.js
@@ -1,22 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
     Table,
     TableBody,
     TableContainer,
     TableHead,
+    TableCell,
     TableFooter,
     TablePagination,
     TableRow,
     Paper,
+    Collapse,
+    Box,
+    Typography,
+    IconButton,
 } from '@mui/material';
-import LaunchIcon from '@mui/icons-material/Launch';
-import { Link } from 'react-router-dom';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import TablePaginationActions from './TablePaginationActions';
 import StyledTableCell from './StyledTableCell';
 import StyledTableRow from './StyledTableRow';
 
 function UsersListTable({ page, rowsPerPage, setUsedParams, rows }) {
+    const [isOpen, setIsOpen] = useState({});
     const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
     const handleChangePage = (event, newPage) => {
@@ -41,8 +47,8 @@ function UsersListTable({ page, rowsPerPage, setUsedParams, rows }) {
             <Table sx={{ minWidth: 700 }} aria-label="customized table">
                 <TableHead>
                     <TableRow>
-                        <StyledTableCell>Käyttäjä (ID)</StyledTableCell>
-                        <StyledTableCell align="right">Nimi</StyledTableCell>
+                        <StyledTableCell> </StyledTableCell>
+                        <StyledTableCell>Nimi</StyledTableCell>
                         <StyledTableCell align="right">Puhelinnumero</StyledTableCell>
                         <StyledTableCell align="right">Sähköposti</StyledTableCell>
                         <StyledTableCell align="right">Oikeudet</StyledTableCell>
@@ -51,18 +57,62 @@ function UsersListTable({ page, rowsPerPage, setUsedParams, rows }) {
                 <TableBody>
                     {(rowsPerPage > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : rows).map(
                         (row) => (
-                            <StyledTableRow key={row.id}>
-                                <StyledTableCell component="th" scope="row">
-                                    <Link to={`/admin/user/${row.id}`}>
-                                        {row.id}
-                                        <LaunchIcon fontSize="small" />
-                                    </Link>
-                                </StyledTableCell>
-                                <StyledTableCell align="right">{row.name}</StyledTableCell>
-                                <StyledTableCell align="right">{row.phone}</StyledTableCell>
-                                <StyledTableCell align="right">{row.email}</StyledTableCell>
-                                <StyledTableCell align="right">{row.roles}</StyledTableCell>
-                            </StyledTableRow>
+                            <>
+                                <StyledTableRow key={row.id}>
+                                    <StyledTableCell component="th" scope="row">
+                                        <IconButton
+                                            aria-label="expand row"
+                                            size="small"
+                                            onClick={() => {
+                                                setIsOpen((prev) => ({ ...prev, [row.id]: !isOpen[row.id] }));
+                                            }}
+                                        >
+                                            {isOpen[row.id] ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                                        </IconButton>
+                                    </StyledTableCell>
+                                    <StyledTableCell>{row.name}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.phone}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.email}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.roles}</StyledTableCell>
+                                </StyledTableRow>
+                                <TableRow>
+                                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                                        <Collapse in={isOpen[row.id]} timeout="auto" unmountOnExit>
+                                            <Box sx={{ margin: 1 }}>
+                                                <Typography variant="h6" gutterBottom component="div">
+                                                    Käyttäjä {row.id}
+                                                </Typography>
+                                                <Table size="small">
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <TableCell component="th" scope="row">
+                                                                Käyttäjä ID
+                                                            </TableCell>
+                                                            <TableCell align="right">Nimi</TableCell>
+                                                            <TableCell align="right">Puhelinnumero</TableCell>
+                                                            <TableCell align="right">Sähköposti</TableCell>
+                                                            <TableCell align="right">Oikeudet</TableCell>
+                                                            <TableCell align="right">Viimeisin kirjautuminen</TableCell>
+                                                        </TableRow>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        <TableRow key={row.id}>
+                                                            <TableCell component="th" scope="row">
+                                                                {row.id}
+                                                            </TableCell>
+                                                            <TableCell align="right">{row.name}</TableCell>
+                                                            <TableCell align="right">{row.phone}</TableCell>
+                                                            <TableCell align="right">{row.email}</TableCell>
+                                                            <TableCell align="right">{row.roles}</TableCell>
+                                                            <TableCell align="right">{row.last_login}</TableCell>
+                                                        </TableRow>
+                                                    </TableBody>
+                                                </Table>
+                                            </Box>
+                                        </Collapse>
+                                    </TableCell>
+                                </TableRow>
+                            </>
                         )
                     )}
 
