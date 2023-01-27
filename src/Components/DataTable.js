da607cc73139e5c7a104c40977d3d4ff575e0cfe
tonipal <86857995+tonipal@users.noreply.github.com>
2023-01-27 12:45:10 +0200

    removed launchIcon for better look

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -10,7 +10,6 @@ import {
     TableRow,
     Paper,
 } from '@mui/material';
-import LaunchIcon from '@mui/icons-material/Launch';
 import { Link } from 'react-router-dom';
 import TablePaginationActions from './TablePaginationActions';
 import StyledTableCell from './StyledTableCell';
@@ -57,10 +56,7 @@ function OrderListTable({ page, rowsPerPage, setUsedParams, rows }) {
                         (row) => (
                             <StyledTableRow key={row.id}>
                                 <StyledTableCell component="th" scope="row">
-                                    <Link to={`/varasto/tilaus/${row.id}?page=0&rows=5`}>
-                                        {row.id}
-                                        <LaunchIcon fontSize="small" />
-                                    </Link>
+                                    <Link to={`/varasto/tilaus/${row.id}?page=0&rows=5`}>{row.id}</Link>
                                 </StyledTableCell>
                                 <StyledTableCell align="right">{row.status}</StyledTableCell>
                                 <StyledTableCell align="right">{row.address}</StyledTableCell>
