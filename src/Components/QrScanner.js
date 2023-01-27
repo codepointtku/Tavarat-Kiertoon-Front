f124982560c23750a0df19db090a2f8e62160676
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-27 12:44:30 +0200

    added navigation

--- src/Components/QrScanner.js
+++ src/Components/QrScanner.js
@@ -1,8 +1,11 @@
+import { useNavigate } from 'react-router';
 import Html5QrcodePlugin from '../Utils/qrcodeScanner';
 
 function QrScanner() {
+    const navigate = useNavigate();
     const onNewScanResult = (decodedText, decodedResult) => {
         console.log(decodedResult, decodedText);
+        navigate(-1, { replace: true, state: decodedText });
     };
     return <Html5QrcodePlugin fps={10} qrbox={250} disableFlip={false} qrCodeSuccessCallback={onNewScanResult} />;
 }
