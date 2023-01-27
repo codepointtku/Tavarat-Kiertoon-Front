5c6e0fdd7c196fbfb91fad152831f093f8e34275
Martin80451 <martin.malmivirtahky@gmail.com>
2023-01-27 12:32:51 +0200

    Changed header size

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -4,7 +4,7 @@ import { Paper, Typography } from '@mui/material';
 function ArticleCard({title, date, content}){
     return(
         <Paper sx={{margin: 5, textAlign: "center"}}>
-            <Typography variant="h4">{title}</Typography> 
+            <Typography variant="h5">{title}</Typography> 
             <Typography variant="subtitle1" sx={{color: '#777777'}}>{date}</Typography>
             <Typography variant="body1">{content}</Typography>
         </Paper>
