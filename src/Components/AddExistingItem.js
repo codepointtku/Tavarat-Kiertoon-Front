73215babbc25f23126c9f47ecab47b3b5e554a60
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-30 11:58:36 +0200

    added buttons and navigation

--- src/Components/AddExistingItem.js
+++ src/Components/AddExistingItem.js
@@ -1,5 +1,32 @@
-function AddExistingItem() {
-    return <h>Add Existing</h>;
+import { useNavigate, generatePath } from 'react-router';
+import PropTypes from 'prop-types';
+import { Button } from '@mui/material';
+
+function AddExistingItem({ item }) {
+    const navigate = useNavigate();
+    return (
+        <>
+            Lisää Esine viivakoodin perusteella
+            <Button
+                onClick={() => navigate(generatePath('/varasto/koodinlukija'), { state: { ...item, oldItem: true } })}
+            >
+                Lue Viivakoodi
+            </Button>
+            Lisää olemassaolevaan esineryhmään
+            <Button>Lue Viivakoodi</Button>
+        </>
+    );
 }
+
+AddExistingItem.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.number,
+        barcode: PropTypes.string,
+        name: PropTypes.string,
+        category: PropTypes.string,
+        location: PropTypes.string,
+        isOld: PropTypes.bool,
+    }).isRequired,
+};
 
 export default AddExistingItem;
