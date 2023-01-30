90b2cbd8256cfb138f694189f3841f0cd8ffe800
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-30 10:34:33 +0200

    added mui lab and basic tabs for addItem

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate, generatePath } from 'react-router';
+import { Box, Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 function AddItem() {
     // empty location.state when item is finally added and site navigates away!
     const location = useLocation();
     const navigate = useNavigate();
     const [item, setItem] = useState();
+    const [tab, setTab] = useState('1');
 
     useEffect(() => {
         if (location.state) {
@@ -27,6 +30,10 @@ function AddItem() {
         }
     }, [location]);
 
+    const tabChange = (event, newValue) => {
+        setTab(newValue);
+    };
+
     console.log(item);
 
     return (
@@ -38,6 +45,20 @@ function AddItem() {
                 Lue viivakoodi
             </button>
             <h1>Add items</h1>
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+                <TabContext value={tab}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={tabChange} aria-label="lab API tabs example">
+                            <Tab label="Item One" value="1" />
+                            <Tab label="Item Two" value="2" />
+                            <Tab label="Item Three" value="3" />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">Item One</TabPanel>
+                    <TabPanel value="2">Item Two</TabPanel>
+                    <TabPanel value="3">Item Three</TabPanel>
+                </TabContext>
+            </Box>
         </>
     );
 }
