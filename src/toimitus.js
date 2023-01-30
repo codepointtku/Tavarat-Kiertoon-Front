03c1bfe43a2daf6445519a4286cf3915c0341d37
craplouse <jaakko.s@hotmail.com>
2023-01-30 14:59:00 +0200

    toimitus & footer styling and links

--- src/toimitus.js
+++ src/toimitus.js
@@ -1,47 +1,48 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
 function Delivery() {
     return (
         <Container fixed>
-            <Typography variant="h4">Toimitus</Typography>
+            <Box sx={{ p: 2 }}>
+                <Typography variant="h4">Toimitus</Typography>
 
-            <Typography variant="caption">Kalustekierron kuljetuksia hoitaa Turun Valmennuspiste</Typography>
-            <br />
-            <Typography variant="caption">Kuljetuksien tiedustelut Vesa Lehtonen, puh. 040 531 8689</Typography>
-            <br />
-            <Typography variant="caption">Osoite: Rieskalähteentie 76, 20300 Turku</Typography>
-            <br />
-            <Typography>
-                Sähköposti: <a href="mailto: tyokeskus.kierratys@turku.fi">tyokeskus.kierratys@turku.fi</a>
-            </Typography>
-            <br />
-            <Typography>Puh: +358 40 531 8689</Typography>
-            <br />
+                <Typography variant="caption">Kalustekierron kuljetuksia hoitaa Turun Valmennuspiste</Typography>
 
-            <Typography variant="h4">Kuljetus</Typography>
-            <Typography variant="caption">
-                Järjestelmästä tilattujen kierrätysuotteiden kuljetuksesta ei peritä erillistä maksua. Asennustöistä
-                voidaan periä työn laajuudesta riippuen lisämaksu.
-            </Typography>
-            <Typography variant="caption">
-                Kuljetus tilataan tuotetilauksen yhteydessä. Vastuu tuotteen kuljetuksesta on tilaajalla.
-            </Typography>
-            <br />
-            <Typography variant="caption">
-                Tarkempia tietoja kuljetuksen kustannuksista ja kuljetusaikatauluista saat ottamalla yhteyttä
-                työkeskukseen.
-            </Typography>
-            <br />
+                <Typography variant="caption">Kuljetuksien tiedustelut Vesa Lehtonen, puh. 040 531 8689</Typography>
 
-            <br />
-            <Typography variant="h4">Askartelumateriaalit</Typography>
-            <Typography variant="h6">Askartelumateriaalien kierrätys on väliaikaisesti tauolla.</Typography>
-            <Typography variant="caption">
-                Askartelumateriaalin tilaukset lähetetään mahdollisuuksien mukaan sisäisellä postilla. Suurempien erien
-                yhteydessä kuljetuksen voi järjestää itse sopimalla noudon askartelumateriaalien varastolta.
-            </Typography>
-            <br />
-            <br />
+                <Typography variant="caption">Osoite: Rieskalähteentie 76, 20300 Turku</Typography>
+
+                <Typography>
+                    Sähköposti: <a href="mailto: tyokeskus.kierratys@turku.fi">tyokeskus.kierratys@turku.fi</a>
+                </Typography>
+
+                <Typography>Puh: +358 40 531 8689</Typography>
+            </Box>
+
+            <Box sx={{ p: 2 }}>
+                <Typography variant="h4">Kuljetus</Typography>
+                <Typography variant="caption">
+                    Järjestelmästä tilattujen kierrätysuotteiden kuljetuksesta ei peritä erillistä maksua. Asennustöistä
+                    voidaan periä työn laajuudesta riippuen lisämaksu.
+                </Typography>
+                <Typography variant="caption">
+                    Kuljetus tilataan tuotetilauksen yhteydessä. Vastuu tuotteen kuljetuksesta on tilaajalla.
+                </Typography>
+
+                <Typography variant="caption">
+                    Tarkempia tietoja kuljetuksen kustannuksista ja kuljetusaikatauluista saat ottamalla yhteyttä
+                    työkeskukseen.
+                </Typography>
+            </Box>
+
+            <Box sx={{ p: 2 }}>
+                <Typography variant="h4">Askartelumateriaalit</Typography>
+                <Typography variant="h6">Askartelumateriaalien kierrätys on väliaikaisesti tauolla.</Typography>
+                <Typography variant="caption">
+                    Askartelumateriaalin tilaukset lähetetään mahdollisuuksien mukaan sisäisellä postilla. Suurempien
+                    erien yhteydessä kuljetuksen voi järjestää itse sopimalla noudon askartelumateriaalien varastolta.
+                </Typography>
+            </Box>
         </Container>
     );
 }
