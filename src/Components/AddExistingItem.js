0e9364b936a37f8418532ce7698c59c878e8b0ea
ErkkoV <erkko.vallivaara@gmail.com>
2023-01-30 12:33:36 +0200

    text-fields and onChange functions

--- src/Components/AddExistingItem.js
+++ src/Components/AddExistingItem.js
@@ -1,19 +1,58 @@
-import { useNavigate, generatePath } from 'react-router';
+import { useState } from 'react';
+import { useNavigate, generatePath, useLocation } from 'react-router';
 import PropTypes from 'prop-types';
-import { Button } from '@mui/material';
+import { Button, TextField, Box } from '@mui/material';
 
 function AddExistingItem({ item }) {
     const navigate = useNavigate();
+    const location = useLocation();
+    const [barCode, setBarCode] = useState();
+    const [itemId, setItemId] = useState();
+
     return (
         <>
-            Lisää Esine viivakoodin perusteella
+            <h3>Lisää Esine viivakoodin lukemisen perusteella</h3>
             <Button
                 onClick={() => navigate(generatePath('/varasto/koodinlukija'), { state: { ...item, oldItem: true } })}
             >
                 Lue Viivakoodi
             </Button>
-            Lisää olemassaolevaan esineryhmään
-            <Button>Lisää Esine</Button>
+            <h3>Lisää esine kirjoitetun viivakoodin tai esineID:n perusteella</h3>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="outlined-basic"
+                    label="TuoteID"
+                    variant="outlined"
+                    onChange={(event) => {
+                        setItemId(event.value);
+                    }}
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Viivakoodi"
+                    variant="outlined"
+                    onChange={(event) => {
+                        setBarCode(event.value);
+                    }}
+                />
+            </Box>
+            <Button
+                onClick={() => {
+                    navigate(generatePath('/varasto/luo'), {
+                        replace: true,
+                        state: { ...location.state, barcode: barCode, id: itemId },
+                    });
+                }}
+            >
+                Lisää Esine
+            </Button>
         </>
     );
 }
