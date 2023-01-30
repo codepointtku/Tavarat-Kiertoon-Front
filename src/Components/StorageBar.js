4c6d744f185077fb818e7a958df183054dfc4853
tonipal <86857995+tonipal@users.noreply.github.com>
2023-01-30 13:14:21 +0200

    StorageBar for navigation

---
+++ src/Components/StorageBar.js
@@ -0,0 +1,39 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function StorageBar() {
+    const pages = [{ name: 'Tilaukset', path: '/varasto/0/delivery?page=0&rows=5' }];
+
+    const navigate = useNavigate();
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/varasto" style={{ color: 'white' }}>
+                            Varasto
+                        </Link>
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                                onClick={() => {
+                                    navigate(`${page.path}`);
+                                }}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
