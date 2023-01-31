1d59d8ec2594785b508df9555ab57b47b9da9a9b
tonipal <86857995+tonipal@users.noreply.github.com>
2023-01-31 14:52:37 +0200

    sub heading to show which page user is on

--- src/Components/StorageBar.js
+++ src/Components/StorageBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,6 +7,8 @@ import Button from '@mui/material/Button';
 import { useNavigate, Link } from 'react-router-dom';
 
 export default function StorageBar() {
+    const [currentPage, setCurrentPage] = useState('Tilaukset');
+
     const pages = [
         { name: 'Tilaukset', path: '/varasto/0/delivery?page=0&rows=5' },
         { name: 'Luo tuotteita', path: '/varasto/luo' },
@@ -18,9 +21,16 @@ export default function StorageBar() {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        <Link to="/varasto" style={{ color: 'white' }}>
+                        <Link
+                            to="/varasto"
+                            style={{ color: 'white' }}
+                            onClick={() => {
+                                setCurrentPage(null);
+                            }}
+                        >
                             Varasto
                         </Link>
+                        <Typography variant="subtitle2">{currentPage}</Typography>
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
                         {pages.map((page) => (
@@ -29,6 +39,7 @@ export default function StorageBar() {
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                                 onClick={() => {
                                     navigate(`${page.path}`);
+                                    setCurrentPage(`${page.name}`);
                                 }}
                             >
                                 {page.name}
