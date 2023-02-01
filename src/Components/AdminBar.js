c3176cc55df2dc1be43b2e27b12a5caafa8fc62b
tonipal <86857995+tonipal@users.noreply.github.com>
2023-02-01 12:57:27 +0200

    useEffect updates currentPage when location change

--- src/Components/AdminBar.js
+++ src/Components/AdminBar.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 
 export default function AdminBar() {
     const [currentPage, setCurrentPage] = useState('Varastot');
@@ -16,19 +16,24 @@ export default function AdminBar() {
     ];
 
     const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === '/admin/users') {
+            setCurrentPage('Käyttäjät');
+        } else if (location.pathname === '/admin/hakemukset') {
+            setCurrentPage('Hakemukset');
+        } else {
+            setCurrentPage('Varastot');
+        }
+    }, [location]);
 
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        <Link
-                            to="/admin"
-                            style={{ color: 'white' }}
-                            onClick={() => {
-                                setCurrentPage('Varastot');
-                            }}
-                        >
+                        <Link to="/admin" style={{ color: 'white' }}>
                             Admin
                         </Link>
                         <Typography variant="subtitle2">{currentPage}</Typography>
@@ -40,7 +45,6 @@ export default function AdminBar() {
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                                 onClick={() => {
                                     navigate(`${page.path}`);
-                                    setCurrentPage(`${page.name}`);
                                 }}
                             >
                                 {page.name}
