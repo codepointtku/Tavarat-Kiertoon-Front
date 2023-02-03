581ef38363ebea06b9982879c77a1479eea71ff6
ErkkoV <erkko.vallivaara@gmail.com>
2023-02-03 12:25:25 +0200

    added faq

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -7,9 +7,17 @@ test.describe('mainpage', () => {
     });
 });
 
+// needs to be changed when errorelement is present
 test.describe('tuotteet', () => {
     test('no errors', async ({ page }) => {
-        await page.goto('/tuotteet');
+        await page.goto('tuotteet/4321');
+        await expect(page.getByText('404 Not Found')).not.toBeVisible();
+    });
+});
+
+test.describe('faq', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('faq/');
         await expect(page.getByText('404 Not Found')).not.toBeVisible();
     });
 });
