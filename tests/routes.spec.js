8e373b00ede316e825bb216e29cbdf39e24d442a
ErkkoV <erkko.vallivaara@gmail.com>
2023-02-03 12:28:19 +0200

    tests for all main routes

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -17,7 +17,35 @@ test.describe('tuotteet', () => {
 
 test.describe('faq', () => {
     test('no errors', async ({ page }) => {
-        await page.goto('faq/');
+        await page.goto('/faq');
+        await expect(page.getByText('404 Not Found')).not.toBeVisible();
+    });
+});
+
+test.describe('delivery', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('/delivery');
+        await expect(page.getByText('404 Not Found')).not.toBeVisible();
+    });
+});
+
+test.describe('backgroundinfo', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('/backgroundinfo');
+        await expect(page.getByText('404 Not Found')).not.toBeVisible();
+    });
+});
+
+test.describe('stats', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('/stats');
+        await expect(page.getByText('404 Not Found')).not.toBeVisible();
+    });
+});
+
+test.describe('tiedotteet', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('/tiedotteet');
         await expect(page.getByText('404 Not Found')).not.toBeVisible();
     });
 });
