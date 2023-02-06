a81c70deb907c145be44d1cb6f5f2c9fd1cd461b
tojoju <35487991+tojoju@users.noreply.github.com>
2023-02-06 07:32:45 +0200

    Modified PDF look, commit to update from dev

--- src/Components/PDFCreator.js
+++ src/Components/PDFCreator.js
@@ -1,50 +1,76 @@
 import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+// import PropTypes from 'prop-types';
+
+// JTo: Disable ESLint proptypes for this file for now
+/* eslint react/prop-types: 0 */
 
 // Create styles
 const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#E4E4E4',
-        border: '1px solid green',
+    orderPage: {
+        flexDirection: 'column',
     },
-    section: {
+    deliveryAddressSection: {
         margin: 10,
         padding: 10,
         flexGrow: 1,
         border: '1px solid red',
+        fontSize: 12,
+        flexDirection: 'row',
+    },
+    deliveryAddressShortSection: {
+        border: '1px solid green',
+        flexDirection: 'column',
+        flexGrow: 1,
+    },
+    deliveryAddressLongSection: {
+        border: '1px solid green',
+        flexDirection: 'column',
+        flexGrow: 2,
+    },
+    deliveryListSection: {
+        margin: 10,
+        padding: 10,
+        flexGrow: 9,
+        border: '1px solid red',
+        fontSize: 12,
     },
 });
 
 // Create Document Component
-/* eslint-disable-next-line */
 function PDFDocument({ order }) {
-    /* eslint-disable-next-line */
-    console.log("PDFCreator", order)
+    console.log('PDFCreator', order);
 
-    /* eslint-disable-next-line */
-    const orderId = order.id
-
-    /* eslint-disable-next-line */
-    const orderRecipient = order.recipient
-
-    /* eslint-disable-next-line */
-    const orderAddress = order.address
+    const orderId = order.id;
+    const orderRecipient = order.recipient;
+    const orderAddress = order.address;
+    const orderProducts = [...order.products];
 
     return (
         <Document>
-            <Page size="A4" style={styles.page}>
-                <View style={styles.section}>
-                <Text>Tilaus {orderId}</Text>
-                <Text>Vastaanottaja {orderRecipient}</Text>
-                <Text>Toimitusosoite {orderAddress}</Text>
+            <Page size="A4" style={styles.orderPage}>
+                <View style={styles.deliveryAddressSection}>
+                    <View style={styles.deliveryAddressShortSection} />
+                    <View style={styles.deliveryAddressShortSection} />
+
+                    <View style={styles.deliveryAddressLongSection}>
+                        <Text>Tilausnumero: {orderId}</Text>
+                        <Text>Vastaanottaja: {orderRecipient}</Text>
+                        <Text>Toimitusosoite: {orderAddress}</Text>
+                    </View>
                 </View>
-                <View style={styles.section}>
-                    <Text>Section #2</Text>
-                    <Text>Jotain tosi tärkeää tietoa</Text>
+                <View style={styles.deliveryListSection}>
+                    <Text>Tuotteet</Text>
+                    {orderProducts.map((id) => (
+                        <Text>- {id}</Text>
+                    ))}
                 </View>
             </Page>
         </Document>
     );
 }
+
+// PDFDocument.propTypes = {
+//     order: PropTypes.object
+// };
 
 export default PDFDocument;
--- src/Components/PDFView.js
+++ src/Components/PDFView.js
@@ -2,13 +2,13 @@ import { PDFViewer } from '@react-pdf/renderer';
 import { useLocation } from 'react-router-dom';
 import PDFDocument from './PDFCreator';
 
-
 function PDFView() {
     const location = useLocation();
+    const { data } = location.state;
 
     return (
         <PDFViewer style={{ width: '100%', height: '1500px', border: '1px solid blue' }}>
-            <PDFDocument order={location.state.data}/>
+            <PDFDocument order={data} />
         </PDFViewer>
     );
 }
