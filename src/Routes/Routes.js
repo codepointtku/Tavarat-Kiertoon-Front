9653a4d93038bf69a839337a6d075bd9f640a834
Palin Valtter <valtter.palin@turku.fi>
2023-02-07 13:09:40 +0200

    created root route (id:"root") to wrap all routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 
 import axios from 'axios';
@@ -38,229 +38,227 @@ function Routes() {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: (
-                <Base>
-                    <DefaultView />
-                </Base>
-            ),
+            element: <Outlet />,
+            id: 'root',
             loader: async () => {
                 const { data } = await axios.get('http://localhost:3001/contacts');
                 return data;
             },
-
             children: [
                 {
                     path: '/',
-                    element: <ProductList />,
-                    loader: async () => {
-                        try {
-                            const { data } = await axios.get('http://localhost:8000/products/');
-                            return data.results;
-                        } catch {
-                            return null;
-                        }
-                    },
-                },
-                {
-                    path: '/tuotteet/:id',
-                    element: <ProductDetails />,
-                    loader: async ({ params }) => {
-                        try {
-                            const { data } = await axios.get(`http://localhost:8000/products/${params.id}`);
-                            return data;
-                        } catch {
-                            return null;
-                        }
-                    },
-                },
-                {
-                    path: '/faq',
-                    element: <FaqView />,
-                },
-                {
-                    path: '/delivery',
-                    element: <Delivery />,
-                    loader: async () => {
-                        const { data } = await axios.get('http://localhost:3001/contacts');
-                        return data;
-                    },
-                },
-                {
-                    path: '/backgroundinfo',
-                    element: <BackgroundInfo />,
-                },
-                {
-                    path: '/stats',
-                    element: <StatsPage />,
-                },
-                {
-                    path: '/tiedotteet',
-                    element: <Announcements />,
-                    loader: async () => {
-                        const { data } = await axios.get('http://localhost:3001/announcements');
-                        try {
-                            return data;
-                        } catch {
-                            return null;
-                        }
-                    },
-                },
-                {
-                    path: '/dummy',
-                    element: <Dummy />,
-                    loader: async () => {
-                        const { data } = await axios.get('http://localhost:3001/categories');
-                        return data;
-                    },
-                },
-            ],
-        },
-        {
-            path: '/varasto',
-            element: (
-                <ThemeProvider theme={storageTheme}>
-                    <Storage>
-                        <StorageView />
-                    </Storage>
-                </ThemeProvider>
-            ),
-            loader: async () => {
-                const { data } = await axios.get('http://localhost:3001/contacts');
-                return data;
-            },
-            children: [
-                {
-                    path: '/varasto/:num/:view',
-                    element: <OrdersList />,
-                    loader: async ({ params }) => {
-                        const { data } = await axios.get('http://localhost:3001/orders');
-                        // num will tell back-end which entries to bring
-                        // view is order status, unless archived can bring all?
-                        // or will be replaced into the back-end later?
-                        const statuses = {
-                            waiting: 2,
-                            delivery: 1,
-                            finished: 0,
-                        };
-                        statuses[params.view] = 10;
-                        data.sort((a, b) => {
-                            if (statuses[a.status] > statuses[b.status]) {
-                                return -1;
-                            }
-                            if (a.status === b.status) {
-                                if (a.id > b.id) {
-                                    return -1;
+                    element: (
+                        <Base>
+                            <DefaultView />
+                        </Base>
+                    ),
+                    children: [
+                        {
+                            path: '/',
+                            element: <ProductList />,
+                            loader: async () => {
+                                try {
+                                    const { data } = await axios.get('http://localhost:8000/products/');
+                                    return data.results;
+                                } catch {
+                                    return null;
                                 }
-                            }
-                            return 1;
-                        });
+                            },
+                        },
+                        {
+                            path: '/tuotteet/:id',
+                            element: <ProductDetails />,
+                            loader: async ({ params }) => {
+                                try {
+                                    const { data } = await axios.get(`http://localhost:8000/products/${params.id}`);
+                                    return data;
+                                } catch {
+                                    return null;
+                                }
+                            },
+                        },
+                        {
+                            path: '/faq',
+                            element: <FaqView />,
+                        },
+                        {
+                            path: '/delivery',
+                            element: <Delivery />,
+                            // loader: async () => {
+                            //     const { data } = await axios.get('http://localhost:3001/contacts');
+                            //     return data;
+                            // },
+                        },
+                        {
+                            path: '/backgroundinfo',
+                            element: <BackgroundInfo />,
+                        },
+                        {
+                            path: '/stats',
+                            element: <StatsPage />,
+                        },
+                        {
+                            path: '/tiedotteet',
+                            element: <Announcements />,
+                            loader: async () => {
+                                const { data } = await axios.get('http://localhost:3001/announcements');
+                                try {
+                                    return data;
+                                } catch {
+                                    return null;
+                                }
+                            },
+                        },
+                        {
+                            path: '/dummy',
+                            element: <Dummy />,
+                            loader: async () => {
+                                const { data } = await axios.get('http://localhost:3001/categories');
+                                return data;
+                            },
+                        },
+                    ],
+                },
+                {
+                    path: '/varasto',
+                    element: (
+                        <ThemeProvider theme={storageTheme}>
+                            <Storage>
+                                <StorageView />
+                            </Storage>
+                        </ThemeProvider>
+                    ),
+                    children: [
+                        {
+                            path: '/varasto/:num/:view',
+                            element: <OrdersList />,
+                            loader: async ({ params }) => {
+                                const { data } = await axios.get('http://localhost:3001/orders');
+                                // num will tell back-end which entries to bring
+                                // view is order status, unless archived can bring all?
+                                // or will be replaced into the back-end later?
+                                const statuses = {
+                                    waiting: 2,
+                                    delivery: 1,
+                                    finished: 0,
+                                };
+                                statuses[params.view] = 10;
+                                data.sort((a, b) => {
+                                    if (statuses[a.status] > statuses[b.status]) {
+                                        return -1;
+                                    }
+                                    if (a.status === b.status) {
+                                        if (a.id > b.id) {
+                                            return -1;
+                                        }
+                                    }
+                                    return 1;
+                                });
 
-                        if (data) {
-                            return data;
-                        }
-                        return null;
-                    },
+                                if (data) {
+                                    return data;
+                                }
+                                return null;
+                            },
+                        },
+                        {
+                            path: '/varasto/tilaus/:id',
+                            element: <OrderView />,
+                            loader: async ({ params }) => {
+                                const { data } = await axios.get(`http://localhost:3001/orders/${params.id}`);
+                                if (data) {
+                                    return data;
+                                }
+                                return null;
+                            },
+                        },
+                        {
+                            path: '/varasto/tilaus/:id/muokkaa',
+                            element: <OrderEdit />,
+                            loader: async ({ params }) => {
+                                const { data } = await axios.get(`http://localhost:3001/orders/${params.id}`);
+                                if (data) {
+                                    return data;
+                                }
+                                return null;
+                            },
+                        },
+                        {
+                            path: '/varasto/luo',
+                            element: <AddItem />,
+                            loader: async () => {
+                                const dataList = [];
+                                let { data } = await axios.get('http://localhost:3001/categories/');
+                                dataList.push(data);
+                                data = await axios.get('http://localhost:3001/storages/');
+                                dataList.push(data.data);
+                                if (dataList) {
+                                    return dataList;
+                                }
+                                return null;
+                            },
+                        },
+                        {
+                            path: '/varasto/koodinlukija',
+                            element: <QrScanner />,
+                        },
+                    ],
                 },
-                {
-                    path: '/varasto/tilaus/:id',
-                    element: <OrderView />,
-                    loader: async ({ params }) => {
-                        const { data } = await axios.get(`http://localhost:3001/orders/${params.id}`);
-                        if (data) {
-                            return data;
-                        }
-                        return null;
-                    },
-                },
-                {
-                    path: '/varasto/tilaus/:id/muokkaa',
-                    element: <OrderEdit />,
-                    loader: async ({ params }) => {
-                        const { data } = await axios.get(`http://localhost:3001/orders/${params.id}`);
-                        if (data) {
-                            return data;
-                        }
-                        return null;
-                    },
-                },
-                {
-                    path: '/varasto/luo',
-                    element: <AddItem />,
-                    loader: async () => {
-                        const dataList = [];
-                        let { data } = await axios.get('http://localhost:3001/categories/');
-                        dataList.push(data);
-                        data = await axios.get('http://localhost:3001/storages/');
-                        dataList.push(data.data);
-                        if (dataList) {
-                            return dataList;
-                        }
-                        return null;
-                    },
-                },
-                {
-                    path: '/varasto/koodinlukija',
-                    element: <QrScanner />,
-                },
-            ],
-        },
-        {
-            path: '/admin',
-            element: (
-                <ThemeProvider theme={adminTheme}>
-                    <Admin>
-                        <AdminView />
-                    </Admin>
-                </ThemeProvider>
-            ),
-            loader: async () => {
-                const { data } = await axios.get('http://localhost:3001/contacts');
-                return data;
-            },
-            children: [
                 {
                     path: '/admin',
-                    element: <StoragesList />,
-                    loader: async () => {
-                        const { data } = await axios.get('http://localhost:3001/storages');
-                        if (data) {
-                            return data;
-                        }
-                        return null;
-                    },
-                },
-                {
-                    path: '/admin/users',
-                    element: <UsersList />,
-                    loader: async () => {
-                        // num will tell back-end which entries to bring
-                        const { data } = await axios.get('http://localhost:3001/users');
-                        // view is order status, unless archived can bring all?
-                        // or will be replaced into the back-end later?
-                        if (data) {
-                            return data;
-                        }
-                        return data;
-                    },
-                },
-                {
-                    path: '/admin/users/:id',
-                    element: <UserEdit />,
-                    loader: async ({ params }) => {
-                        const { data } = await axios.get(`http://localhost:3001/users/${params.id}`);
-                        if (data) {
-                            return data;
-                        }
-                        return data;
-                    },
-                },
-                {
-                    path: '/admin/varastot/:id',
-                    element: <StorageEdit />,
-                },
-                {
-                    path: '/admin/hakemukset',
-                    element: <h2 style={{ textAlign: 'center' }}>Tässä on hakemukset</h2>,
+                    element: (
+                        <ThemeProvider theme={adminTheme}>
+                            <Admin>
+                                <AdminView />
+                            </Admin>
+                        </ThemeProvider>
+                    ),
+                    children: [
+                        {
+                            path: '/admin',
+                            element: <StoragesList />,
+                            loader: async () => {
+                                const { data } = await axios.get('http://localhost:3001/storages');
+                                if (data) {
+                                    return data;
+                                }
+                                return null;
+                            },
+                        },
+                        {
+                            path: '/admin/users',
+                            element: <UsersList />,
+                            loader: async () => {
+                                // num will tell back-end which entries to bring
+                                const { data } = await axios.get('http://localhost:3001/users');
+                                // view is order status, unless archived can bring all?
+                                // or will be replaced into the back-end later?
+                                if (data) {
+                                    return data;
+                                }
+                                return data;
+                            },
+                        },
+                        {
+                            path: '/admin/users/:id',
+                            element: <UserEdit />,
+                            loader: async ({ params }) => {
+                                const { data } = await axios.get(`http://localhost:3001/users/${params.id}`);
+                                if (data) {
+                                    return data;
+                                }
+                                return data;
+                            },
+                        },
+                        {
+                            path: '/admin/varastot/:id',
+                            element: <StorageEdit />,
+                        },
+                        {
+                            path: '/admin/hakemukset',
+                            element: <h2 style={{ textAlign: 'center' }}>Tässä on hakemukset</h2>,
+                        },
+                    ],
                 },
             ],
         },
