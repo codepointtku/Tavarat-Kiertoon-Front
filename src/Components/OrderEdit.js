5c78cef1c81fa819d3263d597bbb26614e858d5f
ErkkoV <erkko.vallivaara@gmail.com>
2023-02-07 08:49:33 +0200

    fixed generatePath

--- src/Components/OrderEdit.js
+++ src/Components/OrderEdit.js
@@ -9,8 +9,8 @@ import {
     TableRow,
     TableBody,
 } from '@mui/material';
-import { useState, useEffect, generatePath } from 'react';
-import { useLoaderData, useLocation, useNavigate } from 'react-router';
+import { useState, useEffect } from 'react';
+import { useLoaderData, useLocation, useNavigate, generatePath } from 'react-router';
 import StyledTableRow from './StyledTableRow';
 import StyledTableCell from './StyledTableCell';
 
