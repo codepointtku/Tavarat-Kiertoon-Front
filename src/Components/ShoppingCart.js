600030c18f6569f7bcbba051a2df0b7aa4367dbe
Martin80451 <martin.malmivirtahky@gmail.com>
2023-02-09 16:01:32 +0200

    Created stepper element.

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,11 +1,28 @@
-import { Container, Box } from '@mui/material';
+import { Container, Box, Stepper, Step, StepLabel } from '@mui/material';
 import { Form } from 'react-router-dom';
 
 function ShoppingCart() {
+    const steps = ['Ostoskori', 'Yhteystiedot & toimitus', 'Vahvistus'];
     return (
-        <Container sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 20, margin: 10 }}>
+        <Container sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 20, margin: '50px 0px' }}>
             <Form>
-                <Box />
+                <Box
+                    sx={{
+                        border: 1,
+                        borderStyle: 'solid',
+                        borderRadius: 3,
+                        borderColor: 'gray',
+                        padding: 10,
+                    }}
+                >
+                    <Stepper alternativeLabel activeStep={0}>
+                        {steps.map((title) => (
+                            <Step key={title}>
+                                <StepLabel>{title}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Box>
             </Form>
         </Container>
     );
