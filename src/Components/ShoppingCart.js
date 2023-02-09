5583bd5734d1d6e8c5d5c9710afff6b85e7c5ccc
Martin80451 <martin.malmivirtahky@gmail.com>
2023-02-09 14:42:29 +0200

    Added styles to container element in ShoppingCart.

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,5 +1,16 @@
-function ShoppingCart(){
-    return <div style={{textAlign: "center", margin: 40}}>Shopping Cart page</div>
+import { Container, Box } from '@mui/material';
+import { Form } from 'react-router-dom';
+
+function ShoppingCart() {
+    return (
+        <Container sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 20, margin: 10 }}>
+            <Form>
+                <Box />
+            </Form>
+        </Container>
+    );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
+
+// return <div style={{ textAlign: 'center', margin: 40 }}>Shopping Cart page</div>;
