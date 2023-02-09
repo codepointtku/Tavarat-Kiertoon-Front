f06ad89fc4b196874d87fb523993e4d42ecd32ff
craplouse <jaakko.s@hotmail.com>
2023-02-09 09:27:24 +0200

    contact form with react hook forms

--- src/Components/ContactPage.js
+++ src/Components/ContactPage.js
@@ -1,15 +1,6 @@
-import {
-    Container,
-    Box,
-    TextField,
-    Typography,
-    Button,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-} from '@mui/material';
+import { Container, TextField, Typography, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import LOGO4 from '../Assets/LOGO4.png';
 
 function TilausNro() {
@@ -27,13 +18,15 @@ function TilausNro() {
 }
 
 function ContactPage() {
-    const [subject, setSubject] = useState('Tilaukset');
+    const [subject, setSubject] = useState('Yleinen palaute');
+    const { register, handleSubmit } = useForm();
+
     const handleChange = (event) => {
         setSubject(event.target.value);
     };
 
     return (
-        <Box
+        <Container
             component="form"
             autoComplete="off"
             sx={{
@@ -48,69 +41,81 @@ function ContactPage() {
             <Typography variant="h3" color="primary.main">
                 Ota yhteyttä
             </Typography>
-
             <Container sx={{ alignItems: 'center' }} maxWidth="md">
-                <TextField
-                    sx={{ mt: 2 }}
-                    id="sender-name"
-                    label="Nimesi"
-                    placeholder="Chuck Norris"
-                    multiline
-                    fullWidth
-                    required
-                    maxRows={2}
-                />
-                <TextField
-                    sx={{ mt: 2 }}
-                    id="sender-email"
-                    label="sähköpostisi"
-                    multiline
-                    fullWidth
-                    required
-                    maxRows={1}
-                    placeholder="chuck.norris@joku.com"
-                />
-                <FormControl fullWidth required sx={{ mt: 2 }}>
-                    <InputLabel id="select-label">Aihe</InputLabel>
-                    <Select
-                        labelId="select-label"
-                        id="simple-select"
-                        value={subject}
-                        label="Aihe"
-                        onChange={handleChange}
+                <FormControl
+                    onSubmit={handleSubmit}
+                    sx={{
+                        marginTop: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <TextField
+                        // eslint-disable-next-line react/jsx-props-no-spreading
+                        {...register('Nimesi')}
+                        sx={{ mt: 2 }}
+                        id="sender-name"
+                        label="Nimesi"
+                        placeholder="Chuck Norris"
+                        multiline
+                        fullWidth
+                        required
+                        maxRows={2}
+                    />
+                    <TextField
+                        sx={{ mt: 2 }}
+                        id="sender-email"
+                        label="sähköpostisi"
+                        multiline
+                        fullWidth
+                        required
+                        maxRows={1}
+                        placeholder="chuck.norris@joku.com"
+                    />
+                    <FormControl fullWidth required sx={{ mt: 2 }}>
+                        <InputLabel id="select-label">Aihe</InputLabel>
+                        <Select
+                            labelId="select-label"
+                            id="simple-select"
+                            value={subject}
+                            label="Aihe"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="Yleinen palaute">Yleinen palaute</MenuItem>
+                            <MenuItem value="Tilaukset">Tilaukset</MenuItem>
+                            <MenuItem value="Tekninen ongelma">Tekninen ongelma</MenuItem>
+                            <MenuItem value="Kehitysehdotukset">Kehitysehdotukset</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    {subject === 'Tilaukset' && <TilausNro />}
+
+                    {/* {subject === 'Tilaukset' ? <TilausNro /> : <Kakka />} */}
+
+                    <TextField
+                        sx={{ mt: 2 }}
+                        placeholder="kerro mikä mieltä painaa"
+                        id="form-message"
+                        label="Viesti"
+                        multiline
+                        required
+                        fullWidth
+                        rows={6}
+                    />
+                    <Button
+                        type="submit"
+                        style={{ width: 200 }}
+                        sx={{
+                            mt: 2,
+                            mb: 2,
+                        }}
                     >
-                        <MenuItem value="Yleinen palaute">Yleinen palaute</MenuItem>
-                        <MenuItem value="Tilaukset">Tilaukset</MenuItem>
-                        <MenuItem value="Tekninen ongelma">Tekninen ongelma</MenuItem>
-                        <MenuItem value="Kehitysehdotukset">Kehitysehdotukset</MenuItem>
-                    </Select>
+                        Lähetä viesti
+                    </Button>
                 </FormControl>
-
-                {subject === 'Tilaukset' && <TilausNro />}
-
-                {/* {subject === 'Tilaukset' ? <TilausNro /> : <Kakka />} */}
-
-                <TextField
-                    sx={{ mt: 2 }}
-                    placeholder="kerro mikä mieltä painaa"
-                    id="form-message"
-                    label="Viesti"
-                    multiline
-                    required
-                    fullWidth
-                    rows={6}
-                />
             </Container>
-            <Button
-                style={{ width: 200 }}
-                sx={{
-                    mt: 2,
-                    mb: 2,
-                }}
-            >
-                NAPPPPPULA
-            </Button>
-        </Box>
+        </Container>
     );
 }
 
