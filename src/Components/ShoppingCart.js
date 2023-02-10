b3ff5aec3bd24762bcf84ebaa35aa2fb60cab809
Martin80451 <martin.malmivirtahky@gmail.com>
2023-02-10 13:58:36 +0200

    Added product information field in cart page.

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,10 +1,18 @@
-import { Container, Box, Stepper, Step, StepLabel } from '@mui/material';
+/* eslint-disable react/jsx-props-no-spreading */
+// import { useState } from 'react';
+
+import { Container, Box, Stepper, Step, StepLabel, Grid, Typography, IconButton } from '@mui/material';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
 import { Form } from 'react-router-dom';
 
 function ShoppingCart() {
+    // const [skipped] = useState(new Set());
     const steps = ['Ostoskori', 'Yhteystiedot & toimitus', 'Vahvistus'];
+
+    // const isStepSkipped = (step) => skipped.has(step);
     return (
-        <Container sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 20, margin: '50px 0px' }}>
+        <Container sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 20, margin: '3.125rem 0rem' }}>
             <Form>
                 <Box
                     sx={{
@@ -13,16 +21,48 @@ function ShoppingCart() {
                         borderRadius: 3,
                         borderColor: 'gray',
                         padding: 10,
+                        marginBottom: 10,
                     }}
                 >
-                    <Stepper alternativeLabel activeStep={0}>
-                        {steps.map((title) => (
-                            <Step key={title}>
-                                <StepLabel>{title}</StepLabel>
-                            </Step>
-                        ))}
+                    <Stepper
+                        sx={{
+                            '& .Mui-disabled': { opacity: 0.5 },
+                        }}
+                        activeStep={0}
+                        alternativeLabel
+                    >
+                        {steps.map((label, index) => {
+                            const stepProps = {};
+                            console.log(index);
+                            // const disabled = index === 0 && true;
+                            // if (isStepSkipped(index)) {
+                            //     stepProps.completed = false;
+                            // }
+                            return (
+                                <Step key={label} {...stepProps}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            );
+                        })}
                     </Stepper>
                 </Box>
+                {/* map tähän myös */}
+                <Grid container direction="row" justifyContent="space-between">
+                    <Grid item>
+                        <Typography variant="h6">Tuotenimi</Typography>
+                    </Grid>
+                    <Box display="inline-flex">
+                        <IconButton color="primary">
+                            <RemoveCircleRoundedIcon />
+                        </IconButton>
+                        <Typography variant="h6">Tuotemäärä</Typography>
+                        <IconButton color="primary">
+                            <AddCircleRoundedIcon />
+                        </IconButton>
+                    </Box>
+                    <Typography variant="h6">Hinta</Typography>
+                </Grid>
+                <hr />
             </Form>
         </Container>
     );
