d9d8c7c4e6a6ae33105103accde948482007edcd
ErkkoV <102948460+ErkkoV@users.noreply.github.com>
2023-02-10 08:53:09 +0200

    restored "/"

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('mainpage', () => {
     test('no errors', async ({ page }) => {
-        await page.goto('');
+        await page.goto('/');
         await expect(page.getByText('Jokin meni pieleen')).not.toBeVisible();
     });
 });
