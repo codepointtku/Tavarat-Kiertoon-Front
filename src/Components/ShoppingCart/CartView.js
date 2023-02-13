54c1b4ab2d88236b347727ae434d5e923e8d25a3
Martin80451 <martin.malmivirtahky@gmail.com>
2023-02-13 15:22:53 +0200

    Moved navigation arrows to separate component.

--- src/Components/ShoppingCart/CartView.js
+++ src/Components/ShoppingCart/CartView.js
@@ -1,11 +1,14 @@
-import { Box, Grid, Typography, IconButton } from '@mui/material';
+import { Form } from 'react-router-dom';
+import { Box, Grid, Typography, IconButton, Button } from '@mui/material';
 
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function CartView() {
     return (
-        <Box>
+        <Form method="put">
             <Grid container direction="row" justifyContent="space-between">
                 <Typography variant="h6">Tuotenimi</Typography>
                 <Box display="inline-flex">
@@ -20,7 +23,15 @@ function CartView() {
                 <Typography variant="h6">Hinta</Typography>
             </Grid>
             <hr />
-        </Box>
+            <Grid container justifyContent="space-between" sx={{ marginTop: 5 }}>
+                <Button type="submit" variant="contained" startIcon={<ArrowBackIcon />}>
+                    Jatka ostoksia
+                </Button>
+                <Button type="submit" variant="contained" endIcon={<ArrowForwardIcon />}>
+                    Seuraava
+                </Button>
+            </Grid>
+        </Form>
     );
 }
 
--- src/Components/ShoppingCart/ShoppingCart.js
+++ src/Components/ShoppingCart/ShoppingCart.js
@@ -1,13 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 // import { useState } from 'react';
-import { Form } from 'react-router-dom';
-import { styled } from '@mui/system';
 
-import { Container, Box, Stepper, Step, StepLabel, Grid, Button } from '@mui/material';
+import { styled } from '@mui/system';
+import { Container, Box, Stepper, Step, StepLabel } from '@mui/material';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 import DomainVerificationIcon from '@mui/icons-material/DomainVerification';
@@ -56,51 +53,41 @@ function ShoppingCart() {
         <Container
             sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, p: 20, paddingBottom: 5, margin: '3.125rem 0rem' }}
         >
-            <Form method="put">
-                <Box
+            <Box
+                sx={{
+                    border: 1,
+                    borderStyle: 'solid',
+                    borderRadius: 3,
+                    borderColor: 'gray',
+                    padding: 10,
+                    marginBottom: 10,
+                }}
+            >
+                <Stepper
                     sx={{
-                        border: 1,
-                        borderStyle: 'solid',
-                        borderRadius: 3,
-                        borderColor: 'gray',
-                        padding: 10,
-                        marginBottom: 10,
+                        '& .Mui-disabled': { opacity: 0.5 },
                     }}
+                    activeStep={1}
+                    connector={<CartStepConnector />}
+                    alternativeLabel
                 >
-                    <Stepper
-                        sx={{
-                            '& .Mui-disabled': { opacity: 0.5 },
-                        }}
-                        activeStep={1}
-                        connector={<CartStepConnector />}
-                        alternativeLabel
-                    >
-                        {steps.map((label, index) => {
-                            const stepProps = {};
-                            console.log(index);
-                            // const disabled = index === 0 && true;
-                            // if (isStepSkipped(index)) {
-                            //     stepProps.completed = false;
-                            // }
-                            return (
-                                <Step key={label} {...stepProps}>
-                                    <StepLabel StepIconComponent={() => iconDecider(index)}>{label}</StepLabel>
-                                </Step>
-                            );
-                        })}
-                    </Stepper>
-                </Box>
-                {/* map tähän myös */}
-                <CartView />
-                <Grid container justifyContent="space-between" sx={{ marginTop: 5 }}>
-                    <Button type="submit" variant="contained" startIcon={<ArrowBackIcon />}>
-                        Jatka ostoksia
-                    </Button>
-                    <Button type="submit" variant="contained" endIcon={<ArrowForwardIcon />}>
-                        Seuraava
-                    </Button>
-                </Grid>
-            </Form>
+                    {steps.map((label, index) => {
+                        const stepProps = {};
+                        console.log(index);
+                        // const disabled = index === 0 && true;
+                        // if (isStepSkipped(index)) {
+                        //     stepProps.completed = false;
+                        // }
+                        return (
+                            <Step key={label} {...stepProps}>
+                                <StepLabel StepIconComponent={() => iconDecider(index)}>{label}</StepLabel>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
+            </Box>
+            {/* map tähän myös */}
+            <CartView />
         </Container>
     );
 }
