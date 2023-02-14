23a477a9a992a251c76a6236817fe61903a902ff
craplouse <jaakko.s@hotmail.com>
2023-02-14 12:54:00 +0200

    updated faq view accordion details and summary

--- src/Components/FaqView.js
+++ src/Components/FaqView.js
@@ -1,10 +1,16 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Grid, Box, Link as MuiLink, Divider } from '@mui/material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+    Grid,
+    Box,
+    Link as MuiLink,
+    AccordionSummary,
+    Divider,
+    Accordion,
+    Typography,
+    AccordionDetails,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function FAQHero() {
     return (
@@ -33,10 +39,15 @@ function FAQHero() {
 }
 
 function FAQAccordions() {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
     return (
         <Grid container spacing={2}>
             <Grid item xs>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -54,7 +65,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel2a-content"
@@ -72,7 +83,7 @@ function FAQAccordions() {
                     </AccordionDetails>
                 </Accordion>
 
-                <Accordion>
+                <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -89,7 +100,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -106,7 +117,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -126,7 +137,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -148,7 +159,7 @@ function FAQAccordions() {
                 </Accordion>
             </Grid>
             <Grid zeroMinWidth item xs>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -169,7 +180,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel8'} onChange={handleChange('panel8')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -189,7 +200,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel9'} onChange={handleChange('panel9')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -208,7 +219,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel10'} onChange={handleChange('panel10')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -229,7 +240,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel11'} onChange={handleChange('panel11')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -248,7 +259,7 @@ function FAQAccordions() {
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-                <Accordion>
+                <Accordion expanded={expanded === 'panel12'} onChange={handleChange('panel12')}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
