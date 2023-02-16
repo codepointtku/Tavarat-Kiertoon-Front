e6adaccd462d067fb12b718dcc9026bb52fed5f2
Martin80451 <martin.malmivirtahky@gmail.com>
2023-02-16 11:41:04 +0200

    Made CartButton component.

---
+++ src/Components/ShoppingCart/CartButton.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { Button, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+function CartButton({ backUrl, forwardUrl, backText, forwardText }) {
+    return (
+        <Grid container justifyContent="space-between" sx={{ marginTop: 5 }}>
+            <Button component={Link} to={backUrl} variant="contained" startIcon={<ArrowBackIcon />}>
+                {backText}
+            </Button>
+            <Button component={Link} to={forwardUrl} type="submit" variant="contained" endIcon={<ArrowForwardIcon />}>
+                {forwardText}
+            </Button>
+        </Grid>
+    );
+}
+
+CartButton.propTypes = {
+    backUrl: PropTypes.string.isRequired,
+    forwardUrl: PropTypes.string.isRequired,
+    backText: PropTypes.string.isRequired,
+    forwardText: PropTypes.string.isRequired,
+};
+
+export default CartButton;
