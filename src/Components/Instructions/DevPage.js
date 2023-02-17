48c6193d2adecd126758698390298140de7e22b2
J. Waananen <j.waananen@protonmail.com>
2023-02-17 11:35:55 +0200

    shipping page template dev

--- src/Components/Instructions/DevPage.js
+++ src/Components/Instructions/DevPage.js
@@ -1,11 +1,11 @@
 // import GuideAccount from './GuideAccount';
 // import GuideBikes from './GuideBikes';
 // import GuideCommon from './GuideCommon';
-import GuideOrdering from './GuideOrdering';
-// import GuideShipping from './GuideShipping';
+// import GuideOrdering from './GuideOrdering';
+import GuideShipping from './GuideShipping';
 
 function DevPage() {
-    return <GuideOrdering />;
+    return <GuideShipping />;
 }
 
 export default DevPage;
--- src/Components/Instructions/GuideShipping.js
+++ src/Components/Instructions/GuideShipping.js
@@ -1,12 +1,57 @@
-import { Typography } from '@mui/material';
+import { Avatar, Box, Container, Typography } from '@mui/material';
+
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+
+function Hero() {
+    return (
+        <Box
+            sx={{
+                mb: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar
+                sx={{
+                    mt: 2,
+                    mb: 1,
+                    bgcolor: 'secondary.dark',
+                    width: 48,
+                    height: 48,
+                }}
+            >
+                <LocalShippingIcon fontSize="large" />
+            </Avatar>
+
+            <Typography variant="h4" color="primary.main">
+                Noutokuljetuksen ohjeet
+            </Typography>
+        </Box>
+    );
+}
+
+function Shipping() {
+    return (
+        <>
+            <Typography variant="h5" color="primary.main" gutterBottom>
+                Noutokuljetuksen tilaaminen
+            </Typography>
+            <Typography gutterBottom>Voit tilata kierrätyskelpoiselle tavarallesi noutokuljetuksen.</Typography>
+            <Typography gutterBottom>
+                Huomioi, jos tavara on huonokuntoista, perimme siitä jätekäsittelymaksun.
+            </Typography>
+        </>
+    );
+}
 
 function GuideShipping() {
     return (
-        <>
-            <Typography>tavis</Typography>
-            <Typography>moro</Typography>
-            <Typography>moro</Typography>
-        </>
+        <Container maxWidth="lg">
+            <Hero />
+            <Shipping />
+        </Container>
     );
 }
 
