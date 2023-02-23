82ea836fd425e6b5859e1c53747e9213d1e0b780
ErkkoV <erkko.vallivaara@gmail.com>
2023-02-23 08:53:01 +0200

    moved orderEdit and storageEdit actions

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,6 @@
 import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
 
-import axios from 'axios';
-
 import DefaultView from './DefaultView';
 import StorageView from './StorageView';
 import AdminView from './AdminView';
@@ -54,7 +52,7 @@ import {
     userSignupLoader,
 } from './loaders';
 
-import { userSignupAction, contactAction } from './actions';
+import { userSignupAction, contactAction, orderEditAction, storageEditAction } from './actions';
 
 import InstructionsPage from '../Components/Instructions/InstructionsPage';
 import GuideCommon from '../Components/Instructions/GuideCommon';
@@ -206,42 +204,7 @@ function Routes() {
                         {
                             path: '/varasto/tilaus/:id/muokkaa',
                             element: <OrderEdit />,
-                            action: async ({ params, request }) => {
-                                const formData = await request.formData();
-                                // const id = Number(formData.get(formData.has('id') ? 'id' : 'index'));
-                                // const productName = formData.get('productName');
-                                if (request.method === 'POST') {
-                                    if (formData.get('type') === 'delete') {
-                                        const response = await axios.delete(
-                                            `http://localhost:8000/orders/${params.id}`,
-                                            {
-                                                data: {
-                                                    product: Number(formData.get('product')),
-                                                    productId: Number(formData.get('productId')),
-                                                },
-                                            }
-                                        );
-                                        if (response.status === 202) {
-                                            return { type: 'delete', status: true };
-                                        }
-                                        return { type: 'delete', status: false };
-                                    }
-                                    if (formData.get('type') === 'put') {
-                                        const response = await axios.put(`http://localhost:8000/orders/${params.id}`, {
-                                            contact: formData.get('contact'),
-                                            delivery_address: formData.get('delivery_address'),
-                                            status: formData.get('status'),
-                                            order_info: formData.get('order_info'),
-                                        });
-                                        if (response.status === 200) {
-                                            return { type: 'update', status: true };
-                                        }
-                                        return { type: 'update', status: false };
-                                    }
-                                }
-
-                                return null;
-                            },
+                            action: orderEditAction,
                             loader: orderEditLoader,
                         },
                         {
@@ -295,26 +258,7 @@ function Routes() {
                         {
                             path: '/admin/varastot/:id',
                             element: <StorageEdit />,
-                            action: async ({ params, request }) => {
-                                const formData = await request.formData();
-                                if (request.method === 'POST') {
-                                    if (formData.get('type') === 'put') {
-                                        const response = await axios.put(
-                                            `http://localhost:8000/storages/${params.id}`,
-                                            {
-                                                address: formData.get('address'),
-                                                name: formData.get('name'),
-                                                in_use: formData.get('in_use'),
-                                            }
-                                        );
-                                        if (response.status === 200) {
-                                            return { type: 'update', status: true };
-                                        }
-                                        return { type: 'update', status: false };
-                                    }
-                                }
-                                return null;
-                            },
+                            action: storageEditAction,
                             loader: storageEditLoader,
                         },
                         {
--- src/Routes/actions.js
+++ src/Routes/actions.js
@@ -25,4 +25,56 @@ const contactAction = async ({ request }) => {
     return response.data || null;
 };
 
-export { userSignupAction, contactAction };
+const orderEditAction = async ({ params, request }) => {
+    const formData = await request.formData();
+    // const id = Number(formData.get(formData.has('id') ? 'id' : 'index'));
+    // const productName = formData.get('productName');
+    if (request.method === 'POST') {
+        if (formData.get('type') === 'delete') {
+            const response = await axios.delete(`http://localhost:8000/orders/${params.id}`, {
+                data: {
+                    product: Number(formData.get('product')),
+                    productId: Number(formData.get('productId')),
+                },
+            });
+            if (response.status === 202) {
+                return { type: 'delete', status: true };
+            }
+            return { type: 'delete', status: false };
+        }
+        if (formData.get('type') === 'put') {
+            const response = await axios.put(`http://localhost:8000/orders/${params.id}`, {
+                contact: formData.get('contact'),
+                delivery_address: formData.get('delivery_address'),
+                status: formData.get('status'),
+                order_info: formData.get('order_info'),
+            });
+            if (response.status === 200) {
+                return { type: 'update', status: true };
+            }
+            return { type: 'update', status: false };
+        }
+    }
+
+    return null;
+};
+
+const storageEditAction = async ({ params, request }) => {
+    const formData = await request.formData();
+    if (request.method === 'POST') {
+        if (formData.get('type') === 'put') {
+            const response = await axios.put(`http://localhost:8000/storages/${params.id}`, {
+                address: formData.get('address'),
+                name: formData.get('name'),
+                in_use: formData.get('in_use'),
+            });
+            if (response.status === 200) {
+                return { type: 'update', status: true };
+            }
+            return { type: 'update', status: false };
+        }
+    }
+    return null;
+};
+
+export { userSignupAction, contactAction, orderEditAction, storageEditAction };
