51ead078bc9b3011dd81d3db4138f589650c2d62
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-02-27 09:48:28 +0200

    Created view for incoming products

---
+++ src/Components/StorageProducts.js
@@ -0,0 +1,32 @@
+import { useLoaderData, useRouteLoaderData } from 'react-router-dom';
+
+import StorageProductsTable from './StorageProductsTable';
+import useCustomSearchParams from '../Hooks/useCustomSearchParams';
+
+function StorageProducts() {
+    const { categories } = useRouteLoaderData('root');
+    const { storages, products } = useLoaderData();
+    const [usedParams, setUsedParams] = useCustomSearchParams({ page: 0, rows: 5 });
+
+    if (!categories) {
+        return <>categories lataus ei toimi.</>;
+    }
+    if (!storages) {
+        return <>storages lataus ei toimi.</>;
+    }
+    if (!products) {
+        return <>products lataus ei toimi.</>;
+    }
+    console.log('categories:', categories);
+    console.log('storages:', storages);
+    console.log('products:', products);
+    console.log('products.results:', products.results);
+
+    return (
+        <div>
+            <StorageProductsTable page={usedParams.page} rowsPerPage={usedParams.rows} setUsedParams={setUsedParams} />
+        </div>
+    );
+}
+
+export default StorageProducts;
