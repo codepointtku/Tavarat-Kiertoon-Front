3c81dad5e7f90eb8f5324089114d4a95faef6d38
J. Waananen <j.waananen@protonmail.com>
2023-03-02 10:34:41 +0200

    backbtn to prod.detailview

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,5 +1,7 @@
+import { useLoaderData, useParams } from 'react-router-dom';
+import { useContext, useState } from 'react';
+
 import {
-    Box,
     Button,
     Card,
     CardActions,
@@ -9,10 +11,12 @@ import {
     ImageList,
     ImageListItem,
     Container,
+    Grid,
 } from '@mui/material';
-import { useLoaderData, useParams } from 'react-router-dom';
+
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { useContext, useState } from 'react';
+
+import BackButton from './BackButton';
 import AuthContext from '../Context/AuthContext';
 
 function ProductDetails() {
@@ -27,70 +31,75 @@ function ProductDetails() {
     const { auth } = useContext(AuthContext);
 
     return (
-        <Box margin={2}>
-            <Container maxWidth="md">
-                <Card>
-                    <CardMedia
-                        component="img"
-                        alt="product image"
-                        height="300"
-                        image={`http://localhost:8000/media/${image}`}
-                    />
-                    <CardContent>
-                        <>
-                            <ImageList cols={6} rowHeight={164}>
-                                {data.pictures.map((pic) => (
-                                    <ImageListItem key={pic} onClick={() => setImage(pic)}>
-                                        <img
-                                            src={`http://localhost:8000/media/${pic}`}
-                                            srcSet={`http://localhost:8000/media/${pic}`}
-                                            alt="kuva"
-                                            loading="lazy"
-                                        />
-                                    </ImageListItem>
-                                ))}
-                            </ImageList>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {productName}
-                            </Typography>
-                            {/* show id if component used in storageview or admin */}
-                            {auth.storage || auth.admin ? (
-                                <Typography variant="body6" color="text.secondary">
-                                    Product id: {productId}
+        <Container maxWidth="md">
+            <Grid container mt={2} mb={2}>
+                <Grid item xs={1}>
+                    <BackButton />
+                </Grid>
+                <Grid item xs={11}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            alt="product image"
+                            height="300"
+                            image={`http://localhost:8000/media/${image}`}
+                        />
+                        <CardContent>
+                            <>
+                                <ImageList cols={6} rowHeight={164}>
+                                    {data.pictures.map((pic) => (
+                                        <ImageListItem key={pic} onClick={() => setImage(pic)}>
+                                            <img
+                                                src={`http://localhost:8000/media/${pic}`}
+                                                srcSet={`http://localhost:8000/media/${pic}`}
+                                                alt="kuva"
+                                                loading="lazy"
+                                            />
+                                        </ImageListItem>
+                                    ))}
+                                </ImageList>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {productName}
                                 </Typography>
-                            ) : null}
+                                {/* show id if component used in storageview or admin */}
+                                {auth.storage || auth.admin ? (
+                                    <Typography variant="body6" color="text.secondary">
+                                        Product id: {productId}
+                                    </Typography>
+                                ) : null}
 
-                            <Typography variant="body1" color="text.secondary" gutterBottom>
-                                Description: {description}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Added on: {new Date(date).toLocaleDateString('fi-FI')}
-                            </Typography>
-                            {/* generate barcode if component used in storageview or admin */}
-                            {auth.storage || auth.admin ? (
-                                <Typography variant="body2" color="text.secondary">
-                                    Barcode: {barcode}
+                                <Typography variant="body1" color="text.secondary" gutterBottom>
+                                    Description: {description}
                                 </Typography>
-                            ) : null}
-                            {/* miten näyttää kategoriat, buttoneina? */}
-                            <Typography variant="body2" color="text.secondary">
-                                Kategoriat:
-                            </Typography>
-                            <Button variant="contained" size="small" disabled>
-                                {/* to be implemented when backend is ready */}
-                                {category}
+                                <Typography variant="body2" color="text.secondary">
+                                    Added on: {new Date(date).toLocaleDateString('fi-FI')}
+                                </Typography>
+                                {/* generate barcode if component used in storageview or admin */}
+                                {auth.storage || auth.admin ? (
+                                    <Typography variant="body2" color="text.secondary">
+                                        Barcode: {barcode}
+                                    </Typography>
+                                ) : null}
+                                {/* miten näyttää kategoriat, buttoneina? */}
+                                <Typography variant="body2" color="text.secondary">
+                                    Kategoriat:
+                                </Typography>
+                                <Button variant="contained" size="small" disabled>
+                                    {/* to be implemented when backend is ready */}
+                                    {category}
+                                </Button>
+                            </>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="medium" aria-label="add to shopping cart">
+                                <AddShoppingCartIcon />
+                                Lisää koriin
                             </Button>
-                        </>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="medium" aria-label="add to shopping cart">
-                            <AddShoppingCartIcon />
-                            Lisää koriin
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Container>
-        </Box>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
 
