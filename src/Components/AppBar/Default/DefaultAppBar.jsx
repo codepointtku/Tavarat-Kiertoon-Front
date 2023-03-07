542b115b07ef3df5525ac8d3f6a54d7236782e81
J. Waananen <j.waananen@protonmail.com>
2023-03-07 14:37:06 +0200

    appbar update

--- src/Components/AppBar/Default/DefaultAppBar.jsx
+++ src/Components/AppBar/Default/DefaultAppBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import {
     AppBar,
@@ -18,6 +18,7 @@ import {
     ListItemIcon,
     ListItemText,
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 // import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
@@ -27,53 +28,10 @@ import MailIcon from '@mui/icons-material/Mail';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import PhishingIcon from '@mui/icons-material/Phishing';
 
-import { Link } from 'react-router-dom';
 import LoginForm from './LoginForm';
 // import ContactForm from './ContactForm';
 
 //
-
-// const Search = styled('div')(({ theme }) => ({
-//     position: 'relative',
-//     borderRadius: theme.shape.borderRadius,
-//     backgroundColor: alpha(theme.palette.common.white, 0.15),
-//     '&:hover': {
-//         backgroundColor: alpha(theme.palette.common.white, 0.25),
-//     },
-//     marginLeft: 0,
-//     width: '100%',
-//     [theme.breakpoints.up('sm')]: {
-//         marginLeft: theme.spacing(1),
-//         width: 'auto',
-//     },
-// }));
-
-// const SearchIconWrapper = styled('div')(({ theme }) => ({
-//     padding: theme.spacing(0, 2),
-//     height: '100%',
-//     position: 'absolute',
-//     pointerEvents: 'none',
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-// }));
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//     color: 'inherit',
-//     '& .MuiInputBase-input': {
-//         padding: theme.spacing(1, 1, 1, 0),
-//         // vertical padding + font size from searchIcon
-//         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//         transition: theme.transitions.create('width'),
-//         width: '100%',
-//         [theme.breakpoints.up('sm')]: {
-//             width: '14ch',
-//             '&:focus': {
-//                 width: '22ch',
-//             },
-//         },
-//     },
-// }));
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
     '& .MuiBadge-badge': {
@@ -88,8 +46,9 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
+    backgroundColor: '#009bd8',
     padding: theme.spacing(0, 1),
-    marginTop: '1rem',
+    // marginTop: '1rem',
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
     justifyContent: 'flex-start',
@@ -147,14 +106,17 @@ function DefaultAppBar() {
     return (
         <Box>
             <AppBar
+                // position="absolute"
                 // color="transparent"
                 sx={{
+                    // top: 0,
+                    // right: 0,
                     zIndex: 1250,
                     // backgroundColor: 'primary.main',
                     width: 'min-content',
                     boxShadow: 0,
                     // padding: '1rem',
-                    marginTop: '1rem',
+                    // marginTop: '1rem',
                 }}
             >
                 <Toolbar
@@ -163,12 +125,6 @@ function DefaultAppBar() {
                     }}
                 >
                     <Stack direction="row" spacing={4}>
-                        {/* <Search>
-                            <SearchIconWrapper>
-                                <SearchIcon sx={{ fontSize: 30, color: '#fff' }} />
-                            </SearchIconWrapper>
-                            <StyledInputBase placeholder="Etsi tuotteita…" inputProps={{ 'aria-label': 'search' }} />
-                        </Search> */}
                         <IconButton onClick={drawerOpen('shoppingCart')}>
                             <StyledBadge
                                 badgeContent={4}
