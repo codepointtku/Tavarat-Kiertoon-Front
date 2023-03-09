75584f847d4a173392855ea897c370f0717a9d7b
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-03-09 14:55:30 +0200

    apiCall fixes, comments for readability

--- src/Utils/apiCall.jsx
+++ src/Utils/apiCall.jsx
@@ -10,15 +10,26 @@ const apiCall = async (auth, setAuth, path, method, data, options) => {
         data,
         options: { ...options },
     });
-    Object.keys(auth).forEach((each) => {
-        if (result.data.authorization?.contains(each)) {
-            if (auth[each] === false) {
-                setAuth({ ...auth, [each]: true });
+    if (path === '/users/login') {
+        console.log('@ apiCall, login data', result.data);
+    }
+
+    if (path === '/users/login/') {
+        // auth on objekti { user_group: false, storage_group: false, admin_group: false }
+        Object.keys(auth).forEach((each) => {
+            // each on joko user_group, storage_group tai admin_group
+            // result.data.group on array, joka sisältää käyttäjän ryhmät [user_group:false, storage_group:false, admin_group:false]
+            if (result.data.groups?.includes(each)) {
+                // esim. jos auth[user_group] on false, eli käyttäjällä ei ole vielä oikeuksia, asetetaan oikeudet
+                if (auth[each] === false) {
+                    setAuth({ ...auth, [each]: true });
+                }
+                // jos on ylimääräisiä oikeuksia, ne poistetaan
+            } else if (auth[each] === true) {
+                setAuth({ ...auth, [each]: false });
             }
-        } else if (auth[each] === true) {
-            setAuth({ ...auth, [each]: false });
-        }
-    });
+        });
+    }
 
     return result;
 };
