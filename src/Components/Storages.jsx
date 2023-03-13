d1ad02d3a4b6f41476ca1ca6c99879e82c7bab3b
ExcavatorZ <taneli.murtola@turku.fi>
2023-03-13 11:25:01 +0200

    Added link inside storage create button.

--- src/Components/Storages.jsx
+++ src/Components/Storages.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { Table, TableBody, TableContainer, TableHead, TableRow, TableCell, Button } from '@mui/material';
 import StyledTableCell from './StyledTableCell';
 
 function Storages({ storages }) {
     const navigate = useNavigate();
+
     return (
         <TableContainer align="center" sx={{ padding: '2rem' }}>
             <Table style={{ width: 1200 }} aria-label="customized table">
@@ -35,7 +36,11 @@ function Storages({ storages }) {
                 </TableBody>
             </Table>
             <h5>
-                <Button onClick={() => navigate('luo')}>Luo varasto</Button>
+                <Button>
+                    <Link to="luo" style={{ color: 'white' }}>
+                        Luo varasto
+                    </Link>
+                </Button>
             </h5>
         </TableContainer>
     );
