112561157e6b967c1941e344512107cf4617c681
Martin80451 <martin.malmivirtahky@gmail.com>
2023-03-13 12:47:41 +0200

    Added sign out button to the welcome section.

--- src/Components/AppBar/Default/Welcome.jsx
+++ src/Components/AppBar/Default/Welcome.jsx
@@ -1,10 +1,15 @@
-import { Typography } from '@mui/material';
+import { Typography, Button, Container } from '@mui/material';
 
 function Welcome({ user }) {
     return (
-        <Typography variant="h4" align="center" color="primary.main" sx={{ mt: 5 }}>
-            Welcome {user}!
-        </Typography>
+        <Container maxWidth="xs">
+            <Typography variant="h4" align="center" color="primary.main" sx={{ mt: 5 }}>
+                Tervetuloa {user}!
+            </Typography>
+            <Button sx={{ mt: 5 }} type="submit" fullWidth>
+                Kirjaudu ulos
+            </Button>
+        </Container>
     );
 }
 
