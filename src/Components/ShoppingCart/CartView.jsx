24747647828ec2b0fc81ea62d9444815abcc7265
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-03-14 15:13:48 +0200

    fix to use routeLoader

--- src/Components/ShoppingCart/CartView.jsx
+++ src/Components/ShoppingCart/CartView.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useRouteLoaderData, useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteLoaderData } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { Box, Grid, Typography, IconButton } from '@mui/material';
 
@@ -11,7 +11,7 @@ import CartButtons from './CartButtons';
 function CartView() {
     const [buttonTask, setButtonTask] = useState('');
     const navigate = useNavigate();
-    const { cartItems } = useRouteLoaderData('root');
+    const { cartItems } = useRouteLoaderData('frontPage');
     const {
         handleSubmit,
         formState: { errors },
@@ -23,17 +23,6 @@ function CartView() {
     };
     console.log({ errors, cartItems });
 
-    // const onSubmit = (data) => console.log(data)
-
-    // const cartItems = [];
-    // cart.products.forEach((i) => {
-    //     cartItems[i.name] = (cartItems[i.name] || 0) + 1;
-    //     // cartItems.unshift({ name: i.name, count: 1 });
-    //     // console.log(itemCount);
-    // });
-
-    // const iterator = cartItems.entries();
-    // console.log(cartItems);
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             {cartItems?.map((item) => (
