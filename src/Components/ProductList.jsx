49daa6d438e61898fbdde74e2000133aaa307cca
J. Waananen <j.waananen@protonmail.com>
2023-03-14 13:48:23 +0200

    fixed frontpage product card rendering

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -7,19 +7,17 @@ function ProductList() {
     const products = useLoaderData();
 
     return (
-        <Grid item xs={7} md={8} lg={9} xl={10}>
-            <Grid container spacing={2}>
-                {products?.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={5} lg={4}>
-                        <ProductCard
-                            id={product.id}
-                            productName={product.name}
-                            date={product.date}
-                            picture={product.pictures[0]}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
+        <Grid container spacing={2}>
+            {products?.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <ProductCard
+                        id={product.id}
+                        productName={product.name}
+                        date={product.date}
+                        picture={product.pictures[0]}
+                    />
+                </Grid>
+            ))}
         </Grid>
     );
 }
