118357dbfac051b8f8115d907458827c6b29af94
J. Waananen <j.waananen@protonmail.com>
2023-03-20 10:13:56 +0200

    code cleanup

--- src/Components/GDPRBox.jsx
+++ src/Components/GDPRBox.jsx
@@ -2,38 +2,28 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Box, Button, Grid, Snackbar, Typography } from '@mui/material';
-import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
 
 import TypographyHeading from './TypographyHeading';
 
 function GDPRText() {
     return (
         <>
-            <TypographyHeading text="Tämä on GDPR-jutska!" />
+            <TypographyHeading text="Tietoa sivuston evästeistä" />
+            <Typography>Käytämme sivustollamme välttämättömiä evästeitä sivuston toiminnallisuuteen.</Typography>
             <Typography>
-                Käytämme sivustollamme evästeitä kerätäksemme ja analysoidaksemme sinun suorituskykyä ja käyttöä sekä
-                tarjotaksemme jakkaroita. Et voi muuttaa suostumustasi milloin tahansa minkä tahansa sivun vasemmassa
-                alakulmassa olevasta hammasrataskuvakkeesta. Älä lue lisää.
+                Jatkaessasi sivuston käyttöä hyväksyt välttämättömien evästeiden tallentamisen selaimen välimuistiin.
             </Typography>
-            <Typography>Hyväksytkö nämä ehdot?</Typography>
         </>
     );
 }
 
 function ConsentButtons() {
     return (
-        <Grid container flexDirection="column" spacing={2}>
-            <Grid item>
-                <Button component={Link} to="/" fullWidth>
-                    Okei
-                </Button>
-            </Grid>
-            <Grid item>
-                <Button component={Link} to="/rickastley" fullWidth>
-                    Ei oo okei
-                </Button>
-            </Grid>
-        </Grid>
+        <Box>
+            <Button component={Link} to="/" fullWidth>
+                Selvä
+            </Button>
+        </Box>
     );
 }
 
@@ -47,9 +37,7 @@ function GDPRBox() {
                     open={open}
                     anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
                     sx={{
-                        '&:hover .MuiBox-root': {
-                            backgroundImage: 'linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)',
-                        },
+                        border: '0.1rem solid #0288d1',
                     }}
                 >
                     <Box
@@ -60,12 +48,10 @@ function GDPRBox() {
                             justifyContent: 'center',
                             alignItems: 'center',
                             backgroundColor: '#fff',
-                            border: '0.2rem solid #0288d1',
                             minWidth: '100vw',
-                            padding: '2rem',
+                            padding: '2rem 8rem 2rem 8rem',
                         }}
                     >
-                        <AccessibleForwardIcon color="primary.main" fontSize="large" />
                         <Grid container justifyContent="center" alignItems="center">
                             <Grid item sm={10}>
                                 <GDPRText />
--- src/Routes/DefaultView.jsx
+++ src/Routes/DefaultView.jsx
@@ -3,12 +3,14 @@ import { Grid } from '@mui/material';
 import SearchField from '../Components/SearchField';
 import CategoryTree from '../Components/CategoryTree';
 import ProductList from '../Components/ProductList';
+import GDPRBox from '../Components/GDPRBox';
 
 // default front page view
 
 function DefaultView() {
     return (
         <>
+            <GDPRBox />
             <SearchField />
             <Grid container spacing={1} mt={2} mb={2}>
                 <Grid item xs={12} sm={4} md={3} lg={2} xl={2}>
