9bc4a36be153010a9bc5aabc1e53170058620e60
Martin80451 <martin.malmivirtahky@gmail.com>
2023-03-23 10:43:46 +0200

    styled and made additions for multiple items input

--- src/Components/AddToCartButton.jsx
+++ src/Components/AddToCartButton.jsx
@@ -2,17 +2,12 @@ import { useState } from 'react';
 import { useSubmit } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import { Box, Button, IconButton, TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 
 function AddToCartButton({ size, id }) {
-    // const { cartItems } = useRouteLoaderData('frontPage');
     const submit = useSubmit();
     const [amount, setAmount] = useState(1);
-    console.log(amount, setAmount);
-
-    // mitä tietoa back endiin menee kun lomake lähetetään
+    console.log(amount);
 
     const handleClickAddToCartBtn = async () => {
         submit(
@@ -24,8 +19,6 @@ function AddToCartButton({ size, id }) {
         );
     };
 
-    // const itemsInCart = cartItems?.filter((item) => item.id === id);
-
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', ml: 2 }}>
             <Button
@@ -36,30 +29,18 @@ function AddToCartButton({ size, id }) {
             >
                 Lisää koriin
             </Button>
-            <IconButton aria-label="vähennä yksi" size="small" color="primary">
-                <RemoveCircleIcon fontSize="small" />
-            </IconButton>
-            <TextField align="center" minWidth={20} />
-            <IconButton aria-label="lisää yksi" size="small" color="primary">
-                <AddCircleIcon fontSize="small" />
-            </IconButton>
+            <TextField
+                align="center"
+                type="number"
+                sx={{ ml: 2 }}
+                inputProps={{ style: { padding: 5, width: 30 } }}
+                value={amount}
+                onChange={(SelectChangeEvent) => {
+                    setAmount(SelectChangeEvent.target.value);
+                }}
+            />
         </Box>
     );
-}
-
-{
-    /* <Input type="number" sx={{ width: 30 }} />; */
-}
-{
-    /* <Button
-                color={itemsInCart?.length > 0 ? 'success' : 'primary'}
-                size={size}
-                aria-label="add to shopping cart"
-                startIcon={<AddShoppingCartOutlinedIcon />}
-                onClick={handleClickAddToCartBtn}
-            >
-                Lisää koriin <br /> {cartItems?.map((item) => item.id === id && `(${item.count}) kpl`)}
-            </Button> */
 }
 
 AddToCartButton.propTypes = {
