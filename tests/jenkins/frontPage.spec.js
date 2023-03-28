7307c37b695409eb0e11c4a85440c75313c3b65e
Vallivaara Erkko <erkko.vallivaara@gmail.com>
2023-03-28 12:47:25 +0300

    added logintest and createNewUser test

---
+++ tests/jenkins/frontPage.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('frontpage', () => {
+    /*     test('login', async ({ page }) => {
+        await page.goto('/');
+        await page.locator('#appbar-containing-div').getByRole('button').nth(1).click();
+        await page.getByPlaceholder('sinä@turku.fi').click();
+        await page.getByPlaceholder('sinä@turku.fi').fill('super');
+        await page.getByPlaceholder('****').click();
+        await page.getByPlaceholder('****').fill('super');
+        await page.getByRole('button', { name: 'Sisään' }).click();
+        await page.getByRole('button', { name: 'Sulje' }).click();
+        await expect(page.getByRole('button', { name: 6 })).toBeVisible();
+    }); */
+
+    test('create new user', async ({ page }) => {
+        await page.goto('/');
+        await page.locator('#appbar-containing-div').getByRole('button').nth(1).click();
+        await page.getByRole('link', { name: 'Luo uusi tunnus' }).click();
+        await page
+            .getByRole('link', { name: 'Käyttäjätili on tarkoitettu henkilökohtaiseksi hankinta-tiliksi.' })
+            .click();
+        await page.getByPlaceholder('sinä@turku.fi').click();
+        await page.getByPlaceholder('sinä@turku.fi').fill('tonipal@turku.fi');
+        await page.getByPlaceholder('Tonipal').click();
+        await page.getByPlaceholder('Tonipal').fill('ToniPal');
+        await page.getByPlaceholder('Kahville').click();
+        await page.getByPlaceholder('Kahville').fill('Kahville');
+        await page.getByPlaceholder('010 111 1111').click();
+        await page.getByPlaceholder('010 111 1111').fill('010101010101');
+        await page.getByPlaceholder('Kahvikuja 5 as. 666').click();
+        await page.getByPlaceholder('Kahvikuja 5 as. 666').fill('Kahvikuja 5 as. 666');
+        await page.getByPlaceholder('20100').click();
+        await page.getByPlaceholder('20100').fill('20100');
+        await page.getByLabel('Kaupunki *').click();
+        await page.getByLabel('Kaupunki *').fill('Turku');
+        await page.getByLabel('Salasana *').click();
+        await page.getByLabel('Salasana *').fill('kahvi');
+        await page.getByLabel('Salasana uudelleen *').click();
+        await page.getByLabel('Salasana uudelleen *').fill('kahvi');
+        await page.getByRole('button', { name: 'Rekisteröidy' }).click();
+        await expect(page.getByText('Tunnuksen luominen onnistui')).toBeVisible();
+    });
+});
