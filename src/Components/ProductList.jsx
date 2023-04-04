74d5c5c4b5bfe26e40a67afcbd2333946db36fda
J. Waananen <j.waananen@protonmail.com>
2023-04-04 12:36:33 +0300

    msg when searchstring returns no results

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,20 +1,31 @@
 import { useLoaderData, useSearchParams } from 'react-router-dom';
 
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Divider, Grid, Typography } from '@mui/material';
+import TypographyHeading from './TypographyHeading';
 import ProductCard from './ProductCard';
 
 function NoSearchResults() {
-    // eslint-disable-next-line no-unused-vars
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const searchQuery = searchParams.get('haku');
 
     return (
-        <Box sx={{ border: '0.1rem solid #bfe6f6', borderRadius: '1rem' }} padding={10}>
-            <Typography component="span">Hakusi "</Typography>
-            <Typography component="span" fontWeight="fontWeightBold">
-                {searchQuery}
+        <Box
+            id="no-results-container"
+            sx={{ padding: '4rem 4rem 10rem 4rem', border: '0.1rem solid #bfe6f6', borderRadius: '1rem' }}
+        >
+            <TypographyHeading text="Hupsista! :-(" />
+            <Box sx={{ margin: '1rem' }}>
+                <Typography component="span">Hakusi “</Typography>
+                <Typography component="span" fontWeight="fontWeightBold">
+                    {searchQuery}
+                </Typography>
+                <Typography component="span">” ei tuottanut tuloksia.</Typography>
+            </Box>
+            <Divider sx={{ margin: '0 0 1rem 0' }} />
+            <Typography variant="body2">
+                Voit tarkistaa oikeinkirjoituksen, kokeilla muita hakusanoja, tai napauttaa tuotekategorioita etsiäksesi
+                tuotteita.
             </Typography>
-            <Typography component="span">" ei tuottanut tuloksia.</Typography>;
         </Box>
     );
 }
@@ -22,27 +33,22 @@ function NoSearchResults() {
 function ProductList() {
     const products = useLoaderData();
 
-    return (
-        // eslint-disable-next-line react/jsx-no-useless-fragment
-        <>
-            {products.length === 0 ? (
-                <NoSearchResults />
-            ) : (
-                <Grid container spacing={2}>
-                    {products?.map((product) => (
-                        <Grid item key={product.id} xs={13} sm={7} md={5} lg={4} xl={3}>
-                            <ProductCard
-                                id={product.id}
-                                groupId={product.group_id}
-                                productName={product.name}
-                                date={product.date}
-                                picture={product.pictures[0]}
-                            />
-                        </Grid>
-                    ))}
+    return products.length ? (
+        <Grid container spacing={2}>
+            {products?.map((product) => (
+                <Grid item key={product.id} xs={13} sm={7} md={5} lg={4} xl={3}>
+                    <ProductCard
+                        id={product.id}
+                        groupId={product.group_id}
+                        productName={product.name}
+                        date={product.date}
+                        picture={product.pictures[0]}
+                    />
                 </Grid>
-            )}
-        </>
+            ))}
+        </Grid>
+    ) : (
+        <NoSearchResults />
     );
 }
 
