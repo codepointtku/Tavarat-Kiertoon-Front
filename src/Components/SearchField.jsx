87048ee384d93046c2568e35a21b395d8e09d14f
J. Waananen <j.waananen@protonmail.com>
2023-04-04 12:37:04 +0300

    searchfield development

--- src/Components/SearchField.jsx
+++ src/Components/SearchField.jsx
@@ -1,15 +1,19 @@
 import { useForm } from 'react-hook-form';
 import { Form, useSearchParams } from 'react-router-dom';
 
-import { Box, Button, InputBase } from '@mui/material';
+import { Box, Button, IconButton, InputBase } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 
 import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useEffect } from 'react';
 
 const Search = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
     position: 'relative',
+    width: '34ch',
+    border: '0.1rem solid #009bd8',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.primary.main, 0.08),
     '&:hover': {
@@ -18,28 +22,54 @@ const Search = styled('div')(({ theme }) => ({
 }));
 
 function SearchField() {
-    const { handleSubmit, register } = useForm();
-    // eslint-disable-next-line no-unused-vars
+    const { handleSubmit, register, watch, reset, setValue } = useForm();
     const [searchParams, setSearchParams] = useSearchParams();
 
     const onSubmit = (formData) => {
         setSearchParams({ haku: formData.search });
     };
 
+    const haku = searchParams.get('haku');
+
+    useEffect(() => {
+        setValue('search', haku);
+    }, [haku]);
+
+    const clearBtnWatcher = watch('search');
+
+    const clearInputField = () => {
+        if (searchParams.has('haku')) {
+            searchParams.delete('haku');
+            setSearchParams(searchParams);
+        } else {
+            reset();
+        }
+    };
+
     return (
-        <Box component={Form} onSubmit={handleSubmit(onSubmit)} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Search>
+        <Box
+            id="searchform"
+            component={Form}
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ display: 'flex', justifyContent: 'center' }}
+        >
+            <Search id="search-wrapper">
+                <SearchIcon sx={{ fontSize: 30, color: 'primary.main', margin: '0 1rem 0 1rem' }} />
                 <InputBase
                     // eslint-disable-next-line react/jsx-props-no-spreading
                     {...register('search')}
                     autoFocus
                     placeholder="Etsi tuotteita…"
-                    startAdornment={<SearchIcon sx={{ fontSize: 30, color: 'primary.main', marginRight: '3rem' }} />}
                     inputProps={{ 'aria-label': 'searchfield' }}
                     sx={{ color: 'inherit', padding: '0.5rem' }}
                 />
+                {clearBtnWatcher?.length > 0 ? (
+                    <IconButton aria-label="clear" onClick={clearInputField} sx={{ marginRight: '1rem' }}>
+                        <ClearIcon />
+                    </IconButton>
+                ) : null}
             </Search>
-            <Button type="submit" sx={{ p: '1rem 2rem 1rem 2rem' }}>
+            <Button id="search-button" type="submit" sx={{ p: '1rem 2rem 1rem 2rem' }}>
                 Hae
             </Button>
         </Box>
