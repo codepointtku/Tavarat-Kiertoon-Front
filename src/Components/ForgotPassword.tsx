77b7d06b8917245b89314c5d2ebece29d7d0f259
Martin80451 <martin.malmivirtahky@gmail.com>
2023-04-17 10:27:48 +0300

    Created component ForgotPassword.

---
+++ src/Components/ForgotPassword.tsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+
+function ForgotPassword() {
+    return <Typography variant="h6">Unohtuiko salasana?</Typography>;
+}
+
+export default ForgotPassword;
