247b3d22c8086b5171a3361775130fdcae595389
Martin80451 <martin.malmivirtahky@gmail.com>
2023-04-18 11:20:59 +0300

    Added content to the password reset page.

--- src/Components/ResetPassword.tsx
+++ src/Components/ResetPassword.tsx
@@ -1,5 +1,33 @@
+import { Container, Box, Grid, Typography, TextField, Button } from '@mui/material';
+
 function ResetPassword() {
-    return <h1>Password reset page</h1>;
+    return (
+        <Container>
+            <Box sx={{ border: 3, borderStyle: 'solid', borderRadius: 3, padding: 5, mt: 5 }}>
+                <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+                    Salasanan palautus
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+                    <Grid direction="column" width={500} gap={2} container>
+                        <Grid item>
+                            <Typography variant="h6">Syötä uusi salasana</Typography>
+                        </Grid>
+                        <Grid item>
+                            <TextField label="Uusi salasana" fullWidth></TextField>
+                        </Grid>
+                        <Grid item>
+                            <TextField label="Uusi salasana uudestaan" fullWidth></TextField>
+                        </Grid>
+                        <Grid item>
+                            <Button type="submit" sx={{ fontWeight: 'fontWeightMediumBold' }}>
+                                Palauta salasana
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
 
 export default ResetPassword;
