fd60efe55e209ae82e253f267eb1d12c6d812816
J. Waananen <j.waananen@protonmail.com>
2023-04-19 10:07:30 +0300

    layout, header template

--- src/Components/Admin/Panel/AdminPanel.tsx
+++ src/Components/Admin/Panel/AdminPanel.tsx
@@ -1,13 +1,17 @@
 import { Box } from '@mui/material';
 
 import NavigationTree from './NavigationTree';
+import PanelHeader from './PanelHeader';
 import ComponentContainer from './ComponentContainer';
 
 function AdminPanel() {
     return (
         <Box id="admin-panel-container" sx={{ border: '1px solid blue', display: 'flex', flexDirection: 'row' }}>
             <NavigationTree />
-            <ComponentContainer />
+            <Box id="keke" sx={{ display: 'flex', flexDirection: 'column' }}>
+                <PanelHeader />
+                <ComponentContainer />
+            </Box>
         </Box>
     );
 }
---
+++ src/Components/Admin/Panel/PanelHeader.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material';
+
+function PanelHeader() {
+    return (
+        <Box id="admin-panel-header" sx={{ border: '1px solid grey' }}>
+            <Box
+                id="header-items-wrapper"
+                sx={{ border: '1px solid pink', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}
+            >
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+                <p>asd</p>
+            </Box>
+        </Box>
+    );
+}
+
+export default PanelHeader;
