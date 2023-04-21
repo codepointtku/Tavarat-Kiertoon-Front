136f9b9e6f8eddfd8d46261014323f1e37275f9c
codepointpetteri <petteri.anttila@turku.fi>
2023-04-21 09:04:04 +0300

    output to file and cleanup output

--- JenkinsReporter.ts
+++ JenkinsReporter.ts
@@ -1,23 +1,23 @@
 import { FullConfig, FullResult, Reporter, Suite, TestCase, TestResult } from '@playwright/test/reporter';
 import config from './playwright.config';
+
+const fs = require('fs');
+const ansiRegex = new RegExp(
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
+    'g'
+);
 class JenkinsReporter implements Reporter {
-    /* onBegin(config: FullConfig, suite: Suite) {
-        console.log(`Starting the run with ${suite.allTests().length} tests`);
-    } */
+    printsToStdio(): boolean {
+        return false;
+    }
 
     onTestEnd(test: TestCase, result: TestResult) {
         // to keep error messages minimal, only print error message after last retry fails.
         if (result.status === 'failed' && result.retry == config.retries) {
-            const message = result.error?.message;
-            console.log(
-                `test "${test.title}" ${result.status}: ${message?.substring(0, message?.indexOf('Call log:'))}`
-            );
+            const message = result.error?.message?.replace(ansiRegex, '');
+            fs.writeFileSync('result.txt', `test "${test.title}" ${result.status}: ${message} `);
         }
     }
-
-    /*  onEnd(result: FullResult) {
-        console.log(`Finished the run: ${result.status}`);
-    } */
 }
 
 export default JenkinsReporter;
