539dbd94eef70c92a552ca3239f8ef0349c80442
Martin80451 <martin.malmivirtahky@gmail.com>
2023-04-21 12:22:19 +0300

    Changed style to be more fitting compared to other pages.

--- src/Components/Default/ResetPassword/ForgotPassword.tsx
+++ src/Components/Default/ResetPassword/ForgotPassword.tsx
@@ -1,7 +1,10 @@
 import { useForm } from 'react-hook-form';
 import { useSubmit, useActionData } from 'react-router-dom';
-import { Typography, Box, Container, TextField, Button, Alert, AlertTitle, Grid } from '@mui/material';
+
+import { Typography, Box, Container, TextField, Button, Alert, Avatar, AlertTitle, Grid } from '@mui/material';
 import KeyIcon from '@mui/icons-material/Key';
+import BackButton from '../../BackButton';
+import TypographyTitle from '../../TypographyTitle';
 
 interface ResponseStatus {
     type: 'string';
@@ -24,53 +27,63 @@ function ForgotPassword() {
     };
 
     return (
-        <Container>
-            <Box
-                sx={{
-                    border: 3,
-                    borderStyle: 'solid',
-                    borderRadius: 3,
-                    padding: 5,
-                    mt: 5,
-                }}
-            >
-                <Grid direction="row" gap={2} alignItems="center" container>
-                    <Grid item>
-                        <KeyIcon fontSize="large" />
+        <Container sx={{ border: '0.1rem solid #bfe6f6', borderRadius: '1rem', pt: 5 }}>
+            <>
+                <Grid container>
+                    <Grid item xs={4}>
+                        <BackButton />
                     </Grid>
-                    <Grid item>
-                        <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
-                            Unohtunut salasana
-                        </Typography>
+                    <Grid
+                        item
+                        xs={4}
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar
+                            sx={{
+                                bgcolor: 'secondary.main',
+                                width: 48,
+                                height: 48,
+                            }}
+                        >
+                            <KeyIcon fontSize="large" />
+                        </Avatar>
                     </Grid>
+                    <Grid item xs={4} />
                 </Grid>
-                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
-                    <Box sx={{ width: 600 }}>
-                        {responseStatus?.status ? (
-                            <Typography variant="h5" sx={{ p: 4, fontWeight: 'fontWeightMediumBold' }}>
-                                Salasanan palautuslinkki lähetetty onnistuneesti!
+                <Box sx={{ marginTop: '1rem', marginBottom: '1rem' }}>
+                    <TypographyTitle text="Unohtunut salasana" />
+                </Box>
+            </>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+                <Box sx={{ width: 600 }}>
+                    {responseStatus?.status ? (
+                        <Typography variant="h5" sx={{ p: 4, fontWeight: 'fontWeightMediumBold' }}>
+                            Salasanan palautuslinkki lähetetty onnistuneesti!
+                        </Typography>
+                    ) : (
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Typography variant="h6" mb={2}>
+                                Syötä käyttäjänimi
                             </Typography>
-                        ) : (
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <Typography variant="h6" mb={2}>
-                                    Syötä käyttäjänimi
-                                </Typography>
-                                <TextField
-                                    label="Käyttäjänimi"
-                                    {...register('username', { required: true, minLength: 4, maxLength: 40 })}
-                                    fullWidth
-                                />
-                                {errors.username && (
-                                    <Alert severity="error" sx={{ mt: 1, maxWidth: 300 }}>
-                                        <AlertTitle>Virheellinen syöte</AlertTitle>
-                                    </Alert>
-                                )}
-                                <Button type="submit" sx={{ mt: 2, fontWeight: 'fontWeightMediumBold' }}>
-                                    Lähetä salasanan palautuslinkki
-                                </Button>
-                            </form>
-                        )}
-                    </Box>
+                            <TextField
+                                label="Käyttäjänimi"
+                                {...register('username', { required: true, minLength: 4, maxLength: 40 })}
+                                fullWidth
+                            />
+                            {errors.username && (
+                                <Alert severity="error" sx={{ mt: 1, maxWidth: 300 }}>
+                                    <AlertTitle>Virheellinen syöte</AlertTitle>
+                                </Alert>
+                            )}
+                            <Button type="submit" sx={{ mt: 2, fontWeight: 'fontWeightMediumBold' }}>
+                                Lähetä salasanan palautuslinkki
+                            </Button>
+                        </form>
+                    )}
                 </Box>
             </Box>
         </Container>
@@ -78,3 +91,16 @@ function ForgotPassword() {
 }
 
 export default ForgotPassword;
+
+{
+    /* <Grid direction="row" gap={2} alignItems="center" container>
+                <Grid item>
+                    <KeyIcon fontSize="large" />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+                        Unohtunut salasana
+                    </Typography>
+                </Grid>
+            </Grid> */
+}
