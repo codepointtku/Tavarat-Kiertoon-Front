b6401129560b9b00b787fe0d31d2b6d9b2f263b2
craplouse <jaakko.s@hotmail.com>
2023-04-28 08:42:25 +0300

    Update BikePackets.tsx

--- src/Components/Bikes/BikePackets.tsx
+++ src/Components/Bikes/BikePackets.tsx
@@ -1,3 +1,33 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import StyledTableCell from '../StyledTableCell';
+
 export default function BikePackets() {
-    return <div>adsadsadasd</div>;
+    return (
+        <TableContainer component={Paper} sx={{ padding: '2rem' }}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell></StyledTableCell>
+                        <StyledTableCell align="right">asd</StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasdasdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasdasdasdad</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <StyledTableCell component="th" scope="row">
+                            asdasd
+                        </StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                        <StyledTableCell align="right">asdasd</StyledTableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
