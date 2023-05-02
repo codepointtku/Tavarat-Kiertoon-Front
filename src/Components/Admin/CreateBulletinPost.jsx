e80e517f41621474515f61bc3a21d33e3df56ab2
J. Waananen <j.waananen@protonmail.com>
2023-05-02 09:19:20 +0300

    bulletincreation refactoring

--- src/Components/Admin/CreateBulletinPost.jsx
+++ src/Components/Admin/CreateBulletinPost.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Form, useSubmit } from 'react-router-dom';
 
-import { Container, TextField, Typography, Button, FormControl, Box } from '@mui/material';
+import { Container, TextField, Button, FormControl, Box, Stack } from '@mui/material';
 
 import AlertBox from '../AlertBox';
 import TypographyTitle from '../TypographyTitle';
@@ -27,58 +27,50 @@ function CreateBulletinPost() {
     };
 
     return (
-        <Box
-            id="bulletin-creation-form-container"
-            component={Form}
-            onSubmit={handleSubmit(onSubmit)}
-            autoComplete="off"
-        >
+        <>
             <TypographyTitle text="Luo uusi tiedote" />
-            <Container sx={{ alignItems: 'center' }} maxWidth="md">
-                <FormControl
-                    id="bulletin-creation-formcontrol"
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <TextField
-                        {...register('title')}
-                        sx={{ mt: 2 }}
-                        label="Otsikko"
-                        placeholder="Otsikko"
-                        fullWidth
-                        inputProps={{ title: 'Otsikko', minLength: '4', maxLength: '50' }}
-                        required
-                    />
+            <Box
+                id="bulletin-creation-form-component"
+                component={Form}
+                onSubmit={handleSubmit(onSubmit)}
+                autoComplete="off"
+            >
+                <Container maxWidth="md">
+                    <Stack id="bulletin-creation-column-stacker">
+                        <FormControl id="bulletin-creation-formcontrol">
+                            <TextField
+                                {...register('title')}
+                                sx={{ mt: 2 }}
+                                label="Otsikko"
+                                placeholder="Otsikko"
+                                fullWidth
+                                inputProps={{ title: 'Otsikko', minLength: '4', maxLength: '50' }}
+                                required
+                            />
 
-                    <TextField
-                        {...register('content')}
-                        sx={{ mt: 2 }}
-                        placeholder="Sisältö"
-                        label="Sisältö"
-                        required
-                        multiline
-                        inputProps={{ minLength: '5' }}
-                        fullWidth
-                        rows={6}
-                    />
+                            <TextField
+                                {...register('content')}
+                                sx={{ mt: 2 }}
+                                label="Sisältö"
+                                placeholder="Sisältö"
+                                multiline
+                                rows={6}
+                                fullWidth
+                                inputProps={{ minLength: '5' }}
+                                required
+                            />
 
-                    <Button disabled={isSubmitting} type="submit" style={{ width: 200 }} sx={{ mt: 2, mb: 2 }}>
-                        LISÄÄ TIEDOTE
-                    </Button>
-                    {success && (
-                        <AlertBox
-                            text="Tiedote lisätty onnistuneesti"
-                            status="success"
-                            redirectUrl="/admin"
-                            timer={1500}
-                        />
-                    )}
-                </FormControl>
-            </Container>
-        </Box>
+                            <Button disabled={isSubmitting} type="submit" sx={{ mt: 2 }}>
+                                Lisää tiedote
+                            </Button>
+                        </FormControl>
+                    </Stack>
+                </Container>
+            </Box>
+            {success && (
+                <AlertBox text="Tiedote lisätty onnistuneesti" status="success" redirectUrl="/admin" timer={1500} />
+            )}
+        </>
     );
 }
 
