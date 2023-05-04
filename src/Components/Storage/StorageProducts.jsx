dfadc773bddaf6f1201715d7458dbbe35e62a8c1
GitPalin <gitpalin@gmail.com>
2023-05-04 13:08:54 +0300

    added qrcode search functionality

--- src/Components/Storage/StorageProducts.jsx
+++ src/Components/Storage/StorageProducts.jsx
@@ -1,11 +1,12 @@
-import { Link, useLoaderData, useRouteLoaderData } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLoaderData, useRouteLoaderData, useSearchParams } from 'react-router-dom';
 
-import { Grid, IconButton, styled } from '@mui/material';
+import { Box, Grid, IconButton, Modal, Typography, styled } from '@mui/material';
+import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
+
 import StorageProductsTable from './StorageProductsTable';
 import AddCircle from '@mui/icons-material/AddCircle';
-// import QrCodeScanner from '@mui/icons-material/QrCodeScanner';
-import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
-// import SearchIcon from '@mui/icons-material/Search';
+import Html5QrcodePlugin from '../../Utils/qrcodeScanner';
 
 const StyledGrid = styled(Grid)(({ theme }) => ({
     display: 'flex',
@@ -16,16 +17,40 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
 }));
 
 function StorageProducts() {
+    const [searchParams, setSearchParams] = useSearchParams();
     const { categories } = useRouteLoaderData('root');
     const { storages, products } = useLoaderData();
+    const [qrSearchOpen, setQrSearchOpen] = useState(false);
 
     console.log('categories:', categories);
     console.log('storages:', storages);
     console.log('products:', products);
     console.log('products.results:', products.results);
 
+    const onNewScanResult = (decodedText, decodedResult) => {
+        setQrSearchOpen(false);
+        setSearchParams({ search: decodedText });
+    };
+
     return (
         <>
+            <Modal
+                open={qrSearchOpen}
+                onClose={() => {
+                    setQrSearchOpen(false);
+                }}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box width={700}>
+                    <Html5QrcodePlugin
+                        fps={10}
+                        qrbox={250}
+                        disableFlip={false}
+                        qrCodeSuccessCallback={onNewScanResult}
+                    />
+                </Box>
+            </Modal>
             <Grid container spacing={4} sx={{ margin: 2 }} justifyContent={'space-evenly'}>
                 <StyledGrid
                     item
@@ -36,14 +61,19 @@ function StorageProducts() {
                         textDecoration: 'none',
                     }}
                 >
-                    <IconButton fontSize="large" color="primary" variant="contained" aria-label="barcode search">
+                    <IconButton fontSize="large" color="primary" variant="contained" aria-label="add new item">
                         <AddCircle sx={{ color: 'primary' }} fontSize="large" />
                         Lisää uusi tuote
                     </IconButton>
                 </StyledGrid>
-                <StyledGrid item xs={3}>
+                <StyledGrid
+                    onClick={() => {
+                        setQrSearchOpen(true);
+                    }}
+                    item
+                    xs={3}
+                >
                     <IconButton fontSize="large" color="primary" variant="contained" aria-label="barcode search">
-                        {/* todo: custom viivakoodi-ikoni? */}
                         <HorizontalSplitIcon
                             sx={{
                                 color: 'primary',
