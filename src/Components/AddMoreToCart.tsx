2131ea26e74a8504ba22b439823ecde229b69665
Martin80451 <martin.malmivirtahky@gmail.com>
2023-05-08 12:22:07 +0300

    Changed addtocartbuttons on shoppin process phase 1

---
+++ src/Components/AddMoreToCart.tsx
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import { useSubmit, useSearchParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import { OverridableStringUnion } from '@material-ui/types';
+import { IconButton, Box, Input, Button, ButtonPropsSizeOverrides } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+interface Props {
+    size?: OverridableStringUnion<'small' | 'medium' | 'large', ButtonPropsSizeOverrides> | undefined;
+    id: number & string;
+    count: number;
+    inOrderingProcess?: boolean | undefined;
+}
+
+function AddMoreToCart({ count, id, size, inOrderingProcess }: Props) {
+    const submit = useSubmit();
+    const [amountN, setAmountN] = useState(1);
+    const [addedToCart, setAddedToCart] = useState(false);
+    const [searchParams] = useSearchParams();
+    const { handleSubmit, register } = useForm();
+
+    function addAmount() {
+        if (amountN === count) {
+            setAmountN(amountN);
+        } else {
+            setAmountN(amountN + 1);
+            setAddedToCart(false);
+        }
+    }
+
+    function removeAmount() {
+        if (amountN === 1) {
+            setAmountN(amountN);
+        } else {
+            setAmountN(amountN - 1);
+            setAddedToCart(false);
+        }
+    }
+
+    function handleOnClick(action: string) {
+        if (amountN >= 1 && amountN <= count) {
+            action === 'add' ? addAmount() : removeAmount();
+        }
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const _input = event.target.value;
+        const input: number = +_input;
+        if ((input >= 1 && input <= count) || _input === '') {
+            setAmountN(Number(input));
+            setAddedToCart(false);
+        }
+    }
+
+    const onSubmit = async () => {
+        const amount = amountN.toString();
+        submit(
+            { id, amount },
+            {
+                method: 'put',
+                action: '/?' + searchParams.toString(),
+            }
+        );
+        setAddedToCart(true);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Box sx={{ display: inOrderingProcess ? 'inline-flex' : 'block' }}>
+                <Box
+                    sx={{
+                        backgroundColor: 'primary.main',
+                        borderRadius: 1,
+                        p: 0.1,
+                        height: 30,
+                        maxWidth: 111,
+                        boxShadow: '0rem 0.05rem 0.2rem 0rem grey',
+                    }}
+                >
+                    <IconButton
+                        size="small"
+                        sx={{ color: 'background.default', padding: 0, mr: 1, ml: 0.5 }}
+                        onClick={() => handleOnClick('remove')}
+                        disabled={amountN === 1}
+                    >
+                        <RemoveIcon />
+                    </IconButton>
+                    <Input
+                        sx={{
+                            mt: 1 / 4,
+                            border: 1,
+                            borderColor: 'white',
+                            backgroundColor: 'white',
+                            color: 'primary.main',
+                            borderRadius: 1,
+                        }}
+                        inputProps={{
+                            style: {
+                                width: 30,
+                                padding: 0,
+                                textAlign: 'center',
+                            },
+                        }}
+                        {...register('amount')}
+                        value={amountN}
+                        onChange={(SelectChangeEvent) => handleChange(SelectChangeEvent)}
+                        disableUnderline
+                    />
+                    <IconButton
+                        size="small"
+                        sx={{ color: 'background.default', padding: 0, ml: 1, mr: 0.5 }}
+                        onClick={() => handleOnClick('add')}
+                        disabled={amountN === count}
+                    >
+                        <AddIcon />
+                    </IconButton>
+                </Box>
+                <Button
+                    size={size}
+                    sx={{ mt: !inOrderingProcess ? 1 / 2 : 0, ml: inOrderingProcess ? 2 : 0 }}
+                    aria-label="add more of same item to shopping cart"
+                    type="submit"
+                    disabled={addedToCart}
+                >
+                    Muuta määrää
+                </Button>
+            </Box>
+        </form>
+    );
+}
+
+export default AddMoreToCart;
--- src/Components/Default/ShoppingCart/CartView.tsx
+++ src/Components/Default/ShoppingCart/CartView.tsx
@@ -1,58 +1,59 @@
-import { useNavigate, useRouteLoaderData, useSubmit } from 'react-router-dom';
+import { useNavigate, useRouteLoaderData } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { Box, Grid, Typography, IconButton } from '@mui/material';
-
-import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+import { Grid, Typography, ButtonPropsSizeOverrides } from '@mui/material';
 
 import CartButtons from './CartButtons';
+import AddMoreToCart from '../../AddMoreToCart';
+import { OverridableStringUnion } from '@material-ui/types';
 import type { shoppingCartLoader } from '../../../Router/loaders';
+import type { productListLoader } from '../../../Router/loaders';
 
 function CartView() {
     const navigate = useNavigate();
-    const submit = useSubmit();
-    const { products } = useRouteLoaderData('frontPage') as Awaited<ReturnType<typeof shoppingCartLoader>>;
+    const { products: cartProducts } = useRouteLoaderData('frontPage') as Awaited<
+        ReturnType<typeof shoppingCartLoader>
+    >;
+    const products = useRouteLoaderData('products') as Awaited<ReturnType<typeof productListLoader>>;
     const { handleSubmit } = useForm();
-
-    const handleClick = (action: string, id: string) => {
-        action === 'add'
-            ? submit({ id }, { method: 'put', action: '/ostoskori/' })
-            : submit({ id }, { method: 'delete', action: '/ostoskori/' });
-    };
 
     const onSubmit = () => {
         navigate('/ostoskori/vaihe2');
     };
 
+    console.log(products);
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <>
             <Grid container direction="row" justifyContent="space-around">
                 <Grid container direction="column" sx={{ width: 'auto' }}>
-                    {products?.map((item: { name: string; id: number }) => (
+                    {cartProducts?.map((item: { name: string; id: number }) => (
                         <Typography variant="h6" sx={{ p: 0.5 }} key={item.id}>
                             {item.name}
                         </Typography>
                     ))}
                 </Grid>
-                <Grid container direction="column" sx={{ width: 'auto' }}>
-                    {products?.map((item: { id: string; count: number }) => (
-                        <Box display="inline-flex" key={item.id}>
-                            <IconButton color="primary" onClick={() => handleClick('remove', item.id)}>
-                                <RemoveCircleRoundedIcon />
-                            </IconButton>
-                            <Typography variant="h6" sx={{ p: 0.5 }}>
-                                {item.count}
-                            </Typography>
-                            <IconButton color="primary" onClick={() => handleClick('add', item.id)}>
-                                <AddCircleRoundedIcon />
-                            </IconButton>
-                        </Box>
+                <Grid container direction="column" gap={1.5} sx={{ width: 'auto' }}>
+                    {cartProducts?.map((item: { id: string & number; count: number }) => (
+                        <AddMoreToCart
+                            key={item.id}
+                            id={item.id}
+                            count={item.count}
+                            size={
+                                'small' as OverridableStringUnion<
+                                    'small' | 'medium' | 'large',
+                                    ButtonPropsSizeOverrides
+                                >
+                            }
+                            inOrderingProcess={true}
+                        />
                     ))}
                 </Grid>
             </Grid>
             <hr />
-            <CartButtons backText="Jatka ostoksia" forwardText="Seuraava" />
-        </form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CartButtons backText="Jatka ostoksia" forwardText="Seuraava" />
+            </form>
+        </>
     );
 }
 
