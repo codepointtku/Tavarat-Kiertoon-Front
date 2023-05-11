e5ee67bc682804d9ae117425d44946e61c9b9802
Martin80451 <martin.malmivirtahky@gmail.com>
2023-05-11 16:02:44 +0300

    Added content.

--- src/Components/Default/Signup/Activation.tsx
+++ src/Components/Default/Signup/Activation.tsx
@@ -1,7 +1,34 @@
-import { Typography } from '@mui/material';
+import { Container, Grid, Button } from '@mui/material';
+import TypographyHeading from '../../TypographyHeading';
+import { Link } from 'react-router-dom';
 
 function Activation() {
-    return <Typography variant="h1">AAAAAAAAAAAAAAA</Typography>;
+    return (
+        <Container onLoad={() => console.log('Testing TESITING')}>
+            <Grid
+                sx={{
+                    border: '0.1rem solid #bfe6f6',
+                    borderRadius: '1rem',
+                    p: 5,
+                    m: 5,
+                    mt: 5,
+                }}
+                container
+                direction="column"
+                alignItems="center"
+                gap={2}
+            >
+                <Grid item>
+                    <TypographyHeading text="Salasana palautettu onnistuneesti" />
+                </Grid>
+                <Grid item>
+                    <Button component={Link} to="/" sx={{ width: 200, p: 2 }}>
+                        Palaa etusivulle
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default Activation;
