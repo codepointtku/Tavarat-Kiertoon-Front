892945948f759b50d8ee3af559e276ea004a8cb1
Martin80451 <martin.malmivirtahky@gmail.com>
2023-05-15 10:55:55 +0300

    Added functionality to mark message as read.

--- src/Components/Default/BulletinCard.tsx
+++ src/Components/Default/BulletinCard.tsx
@@ -1,4 +1,5 @@
-import { Box, Paper, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Box, Paper, Typography, Button, Grid } from '@mui/material';
 
 import TypographyHeading from '../TypographyHeading';
 
@@ -6,12 +7,35 @@ interface Props {
     title: string;
     date: string;
     content: string;
+    isRead?: boolean;
+    setIsRead?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function BulletinCard({ title, date, content }: Props) {
+    const [isRead, setIsRead] = useState(false);
+
     return (
-        <Paper id="bulletin-card-paper-backdrop" elevation={6} sx={{ mb: '2rem', p: '2rem' }}>
-            <TypographyHeading text={title} />
+        <Paper
+            id="bulletin-card-paper-backdrop"
+            elevation={6}
+            sx={{ mb: '2rem', p: '2rem', opacity: isRead ? 0.2 : 1 }}
+        >
+            <Grid container justifyContent="space-between">
+                <Grid item>
+                    <TypographyHeading text={title} />
+                </Grid>
+                {!isRead && (
+                    <Grid item>
+                        <Button onClick={() => setIsRead && setIsRead((isRead) => !isRead)}>
+                            {isRead ? (
+                                <Typography variant="inherit">Merkitse lukemattomaksi</Typography>
+                            ) : (
+                                <Typography variant="inherit">Merkitse luetuksi</Typography>
+                            )}
+                        </Button>
+                    </Grid>
+                )}
+            </Grid>
             <Typography variant="caption" sx={{ color: 'text.hintContrast', mt: '0.5rem' }}>
                 {date}
             </Typography>
