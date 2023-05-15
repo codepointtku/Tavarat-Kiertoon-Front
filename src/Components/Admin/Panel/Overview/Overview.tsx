cf7140986955c349a91acafa753cb213eb389b5c
J. Waananen <j.waananen@protonmail.com>
2023-05-15 10:57:21 +0300

    remove commented code

--- src/Components/Admin/Panel/Overview/Overview.tsx
+++ src/Components/Admin/Panel/Overview/Overview.tsx
@@ -4,22 +4,6 @@ import Summary from './Summary';
 
 function Overview() {
     return (
-        // <Grid id="overview-grid-container" container flexDirection="column">
-        //     <Grid className="overview-grid-item" item>
-        //         you want to ride a pony?
-        //     </Grid>
-        //     <Grid className="overview-grid-item" item>
-        //         <Box className="overview-component" sx={{ border: '1px solid green', padding: '1rem' }}>
-        //             <p>
-        //                 overview-komponenttioverview-komponenttioverview-komponenttioverview-komponenttioverview-komponentti
-        //                 overview-komponentti overview-komponentti overview-komponentti overview-komponentti
-        //                 overview-komponentti overview-komponentti overview-komponentti overview-komponentti
-        //                 overview-komponentti overview-komponentti overview-komponentti overview-komponentti
-        //                 overview-komponentti overview-komponentti
-        //             </p>
-        //         </Box>
-        //     </Grid>
-        // </Grid>
         <Grid id="overview-grid-container" container flexDirection="column">
             <Grid className="overview-grid-item" item>
                 <Summary />
