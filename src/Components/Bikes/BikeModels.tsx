4ca069e351004bb4da8934eb6e68ea2334ad81a1
tojoju <35487991+tojoju@users.noreply.github.com>
2023-05-16 08:22:30 +0300

    UI changes to BikeModels list draft

--- src/Components/Bikes/BikeModels.tsx
+++ src/Components/Bikes/BikeModels.tsx
@@ -21,7 +21,6 @@ import type { bikeModelInterface } from './Bikes';
  */
 function BikeModels() {
     const models = useLoaderData() as bikeModelInterface[];
-    console.log(models);
 
     return (
         <div>
@@ -31,10 +30,8 @@ function BikeModels() {
 
             <Grid
                 container
-                component={Paper}
                 spacing={4}
                 flexDirection="row"
-                paddingRight="2rem"
                 paddingBottom="2rem"
                 justifyContent="center"
                 minWidth="500px"
@@ -44,13 +41,14 @@ function BikeModels() {
                     return (
                         <Grid item key={model.id} xs={6} minWidth="500px">
                             <Card
+                                component={Paper}
                                 sx={{
                                     display: 'flex',
                                     flexDirection: 'column',
                                     border: '0px solid black',
                                     padding: '1rem',
                                 }}
-                                elevation={0}
+                                // elevation={0}
                             >
                                 <Box sx={{ display: 'flex', flexDirection: 'row' }}>
                                     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -60,7 +58,12 @@ function BikeModels() {
                                             image="/br.jpg"
                                             alt="Bike Model"
                                         />
-                                        <Button sx={{ width: '10rem', marginTop: '1rem' }}>Muokkaa</Button>
+                                        <Button
+                                            onClick={() => console.log('Klikkasit "Muokkaa" nappia')}
+                                            sx={{ width: '10rem', marginTop: '1rem' }}
+                                        >
+                                            Muokkaa
+                                        </Button>
                                     </Box>
                                     <Table size="small">
                                         <TableBody>
@@ -83,7 +86,7 @@ function BikeModels() {
                                             <TableRow>
                                                 <TableCell sx={{ fontWeight: 'bold', border: 0 }}>Kuvaus:</TableCell>
                                                 <TableCell colSpan={3} sx={{ border: 0 }}>
-                                                    {model.description} Culpa pariatur cillum consectetur elit occaecat.
+                                                    {model.description}
                                                 </TableCell>
                                             </TableRow>
                                         </TableBody>
