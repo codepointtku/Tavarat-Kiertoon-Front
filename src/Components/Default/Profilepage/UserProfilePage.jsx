3632bf9c36e5697b536d404c2d59a6e6145d28bb
Martin80451 <martin.malmivirtahky@gmail.com>
2023-06-02 10:29:17 +0300

    Added header and navigation tabs.

--- src/Components/Default/Profilepage/UserProfilePage.jsx
+++ src/Components/Default/Profilepage/UserProfilePage.jsx
@@ -1,30 +1,49 @@
 import { useLoaderData } from 'react-router-dom';
 
-import { Grid } from '@mui/material';
+import { Grid, Tabs, Tab, Typography } from '@mui/material';
 
 import ProfileInfo from './ProfileInfo';
 import OrdersActive from './OrdersActive';
 import OrdersHistory from './OrdersHistory';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import TypographyHeading from '../../TypographyHeading';
 
 function UserProfilePage() {
     const { userInfo, userOrders } = useLoaderData();
 
     return (
-        <Grid container flexDirection="row" justifyContent="space-around" sx={{ border: '1px solid blue' }}>
-            <Grid item>
-                <ProfileInfo userInfo={userInfo} />
+        <>
+            <Grid
+                container
+                flexDirection="row"
+                gap={2}
+                sx={{ height: '4rem', backgroundColor: 'primary.main', borderRadius: '0.5rem 0.5rem 0 0', p: 2 }}
+            >
+                <AccountCircleOutlinedIcon sx={{ fontSize: 36, color: '#fff' }} />
+                <Typography variant="h5" color="background.default">
+                    Käyttäjäprofiili
+                </Typography>
             </Grid>
-
-            <Grid flexDirection="column" sx={{ border: '1px solid green' }}>
+            <Tabs centered>
+                <Tab label="Käyttäjätiedot" />
+                <Tab label="Tilaustiedot" />
+            </Tabs>
+            <Grid container flexDirection="row" justifyContent="space-around" sx={{ border: '1px solid blue' }}>
                 <Grid item>
-                    <OrdersActive userOrders={userOrders} />
+                    <ProfileInfo userInfo={userInfo} />
                 </Grid>
 
-                <Grid item>
-                    <OrdersHistory userOrdersHistory={userOrders} />
+                <Grid flexDirection="column" sx={{ border: '1px solid green' }}>
+                    <Grid item>
+                        <OrdersActive userOrders={userOrders} />
+                    </Grid>
+
+                    <Grid item>
+                        <OrdersHistory userOrdersHistory={userOrders} />
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </>
     );
 }
 
