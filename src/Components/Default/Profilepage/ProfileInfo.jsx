7a156eb7adce62713bec3a0963074dcb2b639455
Martin80451 <martin.malmivirtahky@gmail.com>
2023-06-02 14:09:01 +0300

    Added default values and changed layout logic and style.

--- src/Components/Default/Profilepage/ProfileInfo.jsx
+++ src/Components/Default/Profilepage/ProfileInfo.jsx
@@ -1,56 +1,47 @@
-import { Grid, Box, TextField, Typography, Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { Grid, Box, TextField, Typography } from '@mui/material';
 
 function ProfileInfo({ userInfo }) {
     // console.log('ollaan ProfileInfolla', userInfo);
-
     const address = userInfo.address_list.map((item) => item.address);
+
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            username: userInfo.username,
+            firstName: userInfo.first_name,
+            lastName: userInfo.last_name,
+            phoneNumber: userInfo.phone_number,
+            userAddresses: address,
+            userEmail: userInfo.email,
+        },
+    });
 
     return (
         <Box>
             <Typography variant="h5" color="primary.main" align="center" sx={{ mb: 2 }}>
                 Käyttäjäprofiilin tiedot
             </Typography>
-            {/* <TypographyHeading text="Käyttäjäprofiilin tiedot" /> */}
             <Grid container flexDirection="row" justifyContent="space-around">
                 <Grid container flexDirection="column" sx={{ width: 'auto' }} gap={2}>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Käyttäjänimi:</b> {userInfo.username}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('username')} label="Käyttäjänimi" placeholder="Käyttäjänimi" />
                     </Grid>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Etunimi:</b> {userInfo.first_name}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('firstName')} label="Etunimi" placeholder="Etunimi" />
                     </Grid>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Sukunimi:</b> {userInfo.last_name}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('lastName')} label="Sukunimi" placeholder="Sukunimi" />
                     </Grid>
                 </Grid>
                 <Grid container flexDirection="column" sx={{ width: 'auto' }} gap={2}>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Puhelin numero: </b>
-                            {userInfo.phone_number}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('phoneNumber')} label="Puhelin numero" placeholder="Puhelin numero" />
                     </Grid>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Osoite:</b> {address}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('userAddresses')} label="Osoite" placeholder="Osoite" />
                     </Grid>
-                    <Grid justifyContent="space-between" container>
-                        <Typography>
-                            <b>Sähköposti:</b> {userInfo.email}
-                        </Typography>
-                        <Button sx={{ ml: 2 }}>Muokkaa</Button>
+                    <Grid item>
+                        <TextField {...register('userEmail')} label="Sähköposti" placeholder="Sähköposti" disabled />
                     </Grid>
                 </Grid>
             </Grid>
