a317d6626b13b66cc0a314babf9903ee9d4bbce7
Martin80451 <martin.malmivirtahky@gmail.com>
2023-06-07 12:59:51 +0300

    Added order info to OrderCard

--- src/Components/Default/Profilepage/OrderCard.tsx
+++ src/Components/Default/Profilepage/OrderCard.tsx
@@ -1,8 +1,21 @@
 import { Card, CardContent, CardActionArea, CardHeader, Typography, Grid } from '@mui/material';
 
-function OrderCard() {
+interface OrderInfo {
+    orderInfo: {
+        status: string;
+        delivery_address: string;
+        contact: string;
+        order_info: string;
+        delivery_date: null | string;
+        phone_number: string;
+        product_items: [];
+    };
+}
+
+function OrderCard({ orderInfo }: OrderInfo) {
+    console.log(orderInfo);
     return (
-        <Card sx={{ width: 300, height: 150, mt: 2 }} raised>
+        <Card sx={{ maxWidth: 600, minHeight: 150, mt: 2 }} raised>
             <CardActionArea sx={{ height: 'inherit' }}>
                 <CardHeader
                     component={Typography}
@@ -13,25 +26,59 @@ function OrderCard() {
                 />
                 <CardContent>
                     <Grid container direction="column">
-                        <Grid container justifyContent="space-between" direction="row">
-                            <Grid item justifyContent="space-between">
-                                <Typography variant="body2" color="primary.main">
-                                    Tietoa:
+                        <Grid container justifyContent="space-around" direction="row">
+                            <Grid item>
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Tilauksen tila:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.status}
                                 </Typography>
                             </Grid>
                             <Grid item>
-                                <Typography variant="body2" color="primary.main">
-                                    Lisää tietoa:
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Tilauksen tiedot:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.order_info}
                                 </Typography>
                             </Grid>
                         </Grid>
-                        <Grid container justifyContent="space-between" direction="row">
-                            <Grid item></Grid>
-                            <Grid item></Grid>
+                        <Grid container justifyContent="space-around" direction="row">
+                            <Grid item>
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Toimitusosoite:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.delivery_address}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Toimituspäivä:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.delivery_date}
+                                </Typography>
+                            </Grid>
                         </Grid>
-                        <Grid container justifyContent="space-between" direction="row">
-                            <Grid item></Grid>
-                            <Grid item></Grid>
+                        <Grid container justifyContent="space-around" direction="row">
+                            <Grid item>
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Tilaajan sähköposti:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.contact}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body2" sx={{ display: 'inline' }} color="primary.main">
+                                    Tilaajan puhelinnumero:
+                                </Typography>
+                                <Typography variant="body2" sx={{ ml: 0.5, display: 'inline' }}>
+                                    {orderInfo.phone_number}
+                                </Typography>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </CardContent>
--- src/Components/Default/Profilepage/OrdersHistory.jsx
+++ src/Components/Default/Profilepage/OrdersHistory.jsx
@@ -3,11 +3,10 @@ import TypographyHeading from '../../TypographyHeading';
 import OrderCard from './OrderCard';
 
 function OrdersHistory({ userOrdersHistory }) {
-    console.log(userOrdersHistory);
     return (
         <Box sx={{ border: '1px solid red', p: 2 }}>
             <TypographyHeading text="Tilaushistoria" />
-            <OrderCard />
+            <OrderCard orderInfo={userOrdersHistory} />
         </Box>
     );
 }
