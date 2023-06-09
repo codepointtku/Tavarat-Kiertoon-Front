cb8301f1d6b054da961b98f072d1654a045bf72d
Martin80451 <martin.malmivirtahky@gmail.com>
2023-06-09 08:57:19 +0300

    Created basic structure for order page.

--- src/Components/Default/Profilepage/OrderPage.tsx
+++ src/Components/Default/Profilepage/OrderPage.tsx
@@ -1,10 +1,25 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Grid, Box } from '@mui/material';
 // import {CustomizedTime}
 
 function OrderPage() {
     return (
-        <Container>
-            <Typography variant="overline">OrderPage</Typography>
+        <Container sx={{ border: '1px solid red' }} disableGutters>
+            <Grid container id="main-order-separating-grid" direction="row">
+                <Grid xs={6} item>
+                    <Box sx={{ p: 5, border: '1px solid blue' }}>
+                        <Typography variant="overline" align="center">
+                            Osio 1
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid xs={6} item>
+                    <Box sx={{ p: 5, border: '1px solid green' }}>
+                        <Typography variant="overline" align="center">
+                            Osio 2
+                        </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
         </Container>
     );
 }
