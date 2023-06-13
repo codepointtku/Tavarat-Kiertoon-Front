7efa027c160cc67ac35f81b2ec0ce898325cc2e0
Martin80451 <martin.malmivirtahky@gmail.com>
2023-06-13 15:33:20 +0300

    Added textfields

--- src/Components/Default/Profilepage/ModifyAddressInfo.tsx
+++ src/Components/Default/Profilepage/ModifyAddressInfo.tsx
@@ -1,19 +1,30 @@
 import { useLocation } from 'react-router-dom';
 
-import { Box } from '@mui/material';
+import { Box, TextField, Grid } from '@mui/material';
 import HeroHeader from '../../HeroHeader';
 import HeroText from '../../HeroText';
 import BusinessIcon from '@mui/icons-material/Business';
 
 function ModifyAddressInfo() {
-    const { state } = useLocation();
+    const { state: addressInfo } = useLocation();
 
-    console.log(state);
+    console.log(addressInfo);
 
     return (
         <Box sx={{ p: 2 }}>
             <HeroHeader Icon={<BusinessIcon />} />
-            <HeroText title="Muokkaa osoitetietoja" />
+            <HeroText title="Luo uusi osoite" />
+            <Grid container direction="column" alignContent="center" gap={2}>
+                <Grid item>
+                    <TextField label="Osoite" placeholder="Osoite" sx={{ width: '150%' }} />
+                </Grid>
+                <Grid item>
+                    <TextField label="Kaupunki" placeholder="Kaupunki" sx={{ width: '150%' }} />
+                </Grid>
+                <Grid item>
+                    <TextField label="Postinumero" placeholder="Postinumero" sx={{ width: '150%' }} />
+                </Grid>
+            </Grid>
         </Box>
     );
 }
