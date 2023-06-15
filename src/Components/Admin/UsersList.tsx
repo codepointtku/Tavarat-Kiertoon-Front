542c7e432c6a9e051fda5d493c1127d98ac80e58
J. Waananen <j.waananen@protonmail.com>
2023-06-15 15:14:57 +0300

    datagrid localization

--- src/Components/Admin/UsersList.tsx
+++ src/Components/Admin/UsersList.tsx
@@ -12,37 +12,18 @@ import {
 
 import {
     DataGrid,
-    GridRowsProp,
     GridColDef,
-    GridEventListener,
     GridToolbarContainer,
     GridToolbarColumnsButton,
     GridToolbarDensitySelector,
     GridToolbarExport,
     GridToolbarQuickFilter,
     GridToolbarFilterButton,
-    GridRenderCellParams,
 } from '@mui/x-data-grid';
 
 import TypographyTitle from '../TypographyTitle';
 
 import type { usersListLoader } from '../../Router/loaders';
-
-// interface Column {
-//     id: number;
-//     col1: string;
-//     col2: string;
-//     col3: string;
-//     col4: number;
-//     col5: xxx
-// }
-
-// interface User {
-//     id: number;
-//     email: string;
-//     username: string;
-//     phone_number: string;
-// }
 
 function UsersList() {
     const { count, next, previous, results } = useLoaderData() as Awaited<ReturnType<typeof usersListLoader>>;
@@ -57,11 +38,13 @@ function UsersList() {
 
     const columns: GridColDef[] = [
         { field: 'email', headerName: 'Sähköposti', flex: 2 },
-        { field: 'username', headerName: 'Nimi', flex: 1 },
+        { field: 'username', headerName: 'Käyttäjänimi', flex: 1 },
+        { field: 'first_name', headerName: 'Etunimi' },
+        { field: 'last_name', headerName: 'Sukunimi' },
         { field: 'phone_number', headerName: 'Puhelinnumero', flex: 1 },
         {
             field: 'id',
-            headerName: 'Lisätiedot',
+            headerName: 'Toiminnot',
             renderCell: (params) => (
                 <Button variant="outlined">
                     <MuiLink component={Link} to={`/admin/kayttajat/${params.value}`}>
@@ -74,11 +57,187 @@ function UsersList() {
 
     if (!results) return null;
 
+    const localizedTextsMap = {
+        // https://github.com/mui/mui-x/blob/master/packages/grid/x-data-grid/src/locales/fiFI.ts
+        // Root
+        noRowsLabel: 'Ei rivejä',
+        noResultsOverlayLabel: 'Ei tuloksia.',
+
+        // Density selector toolbar button text
+        toolbarDensity: 'Rivit',
+        toolbarDensityLabel: 'Riviväli',
+        toolbarDensityCompact: 'Kompakti',
+        toolbarDensityStandard: 'Vakio',
+        toolbarDensityComfortable: 'Tilava',
+
+        // Columns selector toolbar button text
+        toolbarColumns: 'Kolumnit',
+        toolbarColumnsLabel: 'Valitse kolumnit',
+
+        // Filters toolbar button text
+        toolbarFilters: 'Suodattimet',
+        toolbarFiltersLabel: 'Näytä suodattimet',
+        toolbarFiltersTooltipHide: 'Piilota suodattimet',
+        toolbarFiltersTooltipShow: 'Näytä suodattimet',
+        toolbarFiltersTooltipActive: (count: any) =>
+            count !== 1 ? `${count} aktiivista suodatinta` : `${count} aktiivinen suodatin`,
+
+        // Quick filter toolbar field
+        toolbarQuickFilterPlaceholder: 'Hae…',
+        toolbarQuickFilterLabel: 'Hae',
+        toolbarQuickFilterDeleteIconLabel: 'Tyhjennä',
+
+        // Export selector toolbar button text
+        toolbarExport: 'Vie',
+        toolbarExportLabel: 'Vie',
+        toolbarExportCSV: 'Lataa CSV-muodossa',
+        toolbarExportPrint: 'Tulosta',
+        toolbarExportExcel: 'Lataa Excel-muodossa',
+
+        // Columns panel text
+        columnsPanelTextFieldLabel: 'Etsi kolumni',
+        columnsPanelTextFieldPlaceholder: 'Kolumnin otsikko',
+        columnsPanelDragIconLabel: 'Järjestä kolumni uudelleen',
+        columnsPanelShowAllButton: 'Näytä kaikki',
+        columnsPanelHideAllButton: 'Piilota kaikki',
+
+        // Filter panel text
+        filterPanelAddFilter: 'Lisää suodatin',
+        // filterPanelRemoveAll: 'Remove all',
+        filterPanelDeleteIconLabel: 'Poista',
+        filterPanelLogicOperator: 'Logiikkaoperaattori',
+        filterPanelOperator: 'Operaattorit',
+        filterPanelOperatorAnd: 'Ja',
+        filterPanelOperatorOr: 'Tai',
+        filterPanelColumns: 'Kolumnit',
+        filterPanelInputLabel: 'Arvo',
+        filterPanelInputPlaceholder: 'Suodattimen arvo',
+
+        // Filter operators text
+        filterOperatorContains: 'sisältää',
+        filterOperatorEquals: 'on yhtä suuri',
+        filterOperatorStartsWith: 'alkaa',
+        filterOperatorEndsWith: 'päättyy',
+        filterOperatorIs: 'on',
+        filterOperatorNot: 'ei ole',
+        filterOperatorAfter: 'on jälkeen',
+        filterOperatorOnOrAfter: 'on sama tai jälkeen',
+        filterOperatorBefore: 'on ennen',
+        filterOperatorOnOrBefore: 'on sama tai ennen',
+        filterOperatorIsEmpty: 'on tyhjä',
+        filterOperatorIsNotEmpty: 'ei ole tyhjä',
+        filterOperatorIsAnyOf: 'mikä tahansa seuraavista',
+        // 'filterOperator=': '=',
+        // 'filterOperator!=': '!=',
+        // 'filterOperator>': '>',
+        // 'filterOperator>=': '>=',
+        // 'filterOperator<': '<',
+        // 'filterOperator<=': '<=',
+
+        // Header filter operators text
+        // headerFilterOperatorContains: 'Contains',
+        // headerFilterOperatorEquals: 'Equals',
+        // headerFilterOperatorStartsWith: 'Starts with',
+        // headerFilterOperatorEndsWith: 'Ends with',
+        // headerFilterOperatorIs: 'Is',
+        // headerFilterOperatorNot: 'Is not',
+        // headerFilterOperatorAfter: 'Is after',
+        // headerFilterOperatorOnOrAfter: 'Is on or after',
+        // headerFilterOperatorBefore: 'Is before',
+        // headerFilterOperatorOnOrBefore: 'Is on or before',
+        // headerFilterOperatorIsEmpty: 'Is empty',
+        // headerFilterOperatorIsNotEmpty: 'Is not empty',
+        // headerFilterOperatorIsAnyOf: 'Is any of',
+        // 'headerFilterOperator=': 'Equals',
+        // 'headerFilterOperator!=': 'Not equals',
+        // 'headerFilterOperator>': 'Greater than',
+        // 'headerFilterOperator>=': 'Greater than or equal to',
+        // 'headerFilterOperator<': 'Less than',
+        // 'headerFilterOperator<=': 'Less than or equal to',
+
+        // Filter values text
+        filterValueAny: 'mikä tahansa',
+        filterValueTrue: 'tosi',
+        filterValueFalse: 'epätosi',
+
+        // Column menu text
+        columnMenuLabel: 'Valikko',
+        columnMenuShowColumns: 'Näytä kolumnit',
+        // columnMenuManageColumns: 'Manage columns',
+        columnMenuFilter: 'Suodata',
+        columnMenuHideColumn: 'Piilota',
+        columnMenuUnsort: 'Poista järjestys',
+        columnMenuSortAsc: 'Järjestä nousevasti',
+        columnMenuSortDesc: 'Järjestä laskevasti',
+
+        // Column header text
+        columnHeaderFiltersTooltipActive: (count: any) =>
+            count !== 1 ? `${count} aktiivista suodatinta` : `${count} aktiivinen suodatin`,
+        columnHeaderFiltersLabel: 'Näytä suodattimet',
+        columnHeaderSortIconLabel: 'Järjestä',
+
+        // Rows selected footer text
+        footerRowSelected: (count: any) =>
+            count !== 1 ? `${count.toLocaleString()} riviä valittu` : `${count.toLocaleString()} rivi valittu`,
+
+        // Total row amount footer text
+        footerTotalRows: 'Rivejä yhteensä:',
+
+        // Total visible row amount footer text
+        footerTotalVisibleRows: (visibleCount: any, totalCount: any) =>
+            `${visibleCount.toLocaleString()} / ${totalCount.toLocaleString()}`,
+
+        // Checkbox selection text
+        checkboxSelectionHeaderName: 'Valintaruutu',
+        checkboxSelectionSelectAllRows: 'Valitse kaikki rivit',
+        checkboxSelectionUnselectAllRows: 'Poista kaikkien rivien valinta',
+        checkboxSelectionSelectRow: 'Valitse rivi',
+        checkboxSelectionUnselectRow: 'Poista rivin valinta',
+
+        // Boolean cell text
+        booleanCellTrueLabel: 'tosi',
+        booleanCellFalseLabel: 'epätosi',
+
+        // Actions cell more text
+        actionsCellMore: 'lisää',
+
+        // Column pinning text
+        pinToLeft: 'Kiinnitä vasemmalle',
+        pinToRight: 'Kiinnitä oikealle',
+        unpin: 'Irrota kiinnitys',
+
+        // Tree Data
+        treeDataGroupingHeaderName: 'Ryhmä',
+        treeDataExpand: 'Laajenna',
+        treeDataCollapse: 'Supista',
+
+        // Grouping columns
+        groupingColumnHeaderName: 'Ryhmä',
+        groupColumn: (name: any) => `Ryhmittelyperuste ${name}`,
+        unGroupColumn: (name: any) => `Poista ryhmittelyperuste ${name}`,
+
+        // Master/detail
+        detailPanelToggle: 'Yksityiskohtapaneelin vaihto',
+        expandDetailPanel: 'Laajenna',
+        collapseDetailPanel: 'Tiivistä',
+
+        // Row reordering text
+        rowReorderingHeaderName: 'Rivien uudelleenjärjestely',
+
+        // Aggregation
+        aggregationMenuItemHeader: 'Koostaminen',
+        aggregationFunctionLabelSum: 'summa',
+        aggregationFunctionLabelAvg: 'ka.',
+        aggregationFunctionLabelMin: 'min.',
+        aggregationFunctionLabelMax: 'maks.',
+        aggregationFunctionLabelSize: 'koko',
+    };
+
     const GridX = () => {
         return (
             <div style={{ height: 500 }}>
                 <DataGrid
-                    paginationMode={'server'}
+                    // paginationMode={'server'}
                     // rowCount={pageCount}
                     rowCount={rowCountState}
                     rows={results}
@@ -88,17 +247,19 @@ function UsersList() {
                             return (
                                 <GridToolbarContainer sx={{ justifyContent: 'flex-end', marginBottom: '1rem' }}>
                                     <GridToolbarQuickFilter />
-                                    <GridToolbarColumnsButton />
                                     <GridToolbarFilterButton />
+                                    <GridToolbarColumnsButton />
                                     <GridToolbarDensitySelector />
                                     <GridToolbarExport />
                                 </GridToolbarContainer>
                             );
                         },
                     }}
-                    // checkboxSelection
+                    checkboxSelection
+                    localeText={localizedTextsMap}
                     // showColumnVerticalBorder
                     // showCellVerticalBorder
+                    // loading
                 />
             </div>
         );
