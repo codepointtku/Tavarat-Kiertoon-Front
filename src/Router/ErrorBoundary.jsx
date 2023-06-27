cfe358541f0456f6477f9001a822e72c0fcdb51a
J. Waananen <j.waananen@protonmail.com>
2023-06-27 12:11:05 +0300

    some err messages

--- src/Router/ErrorBoundary.jsx
+++ src/Router/ErrorBoundary.jsx
@@ -6,14 +6,26 @@ const errorType = (err) => {
     if (err?.response?.status === 204) {
         return 'nocontent';
     }
-    if (err?.status === 404) {
-        return 'noroute';
-    }
     if (err?.status === 400) {
         return 'badrequest';
     }
+    if (err?.status === 401) {
+        return 'unauthorized';
+    }
+    if (err?.status === 403) {
+        return 'forbidden';
+    }
+    if (err?.status === 404) {
+        return 'noroute';
+    }
+    if (err?.status === 418) {
+        return 'teapot';
+    }
     if (err?.response?.status === 500) {
         return 'internalservererror';
+    }
+    if (err?.status === 503) {
+        return 'serviceunavailable';
     }
     return 'else';
 };
@@ -29,13 +41,22 @@ function ErrorBoundary() {
 
     const errorTypes = {
         nocontent: <Typography variant="h6">No content @ {location.pathname}</Typography>,
-        noroute: <Typography variant="h6">Etsimääsi sijaintia {location.pathname} ei valitettavasti löydy.</Typography>,
         badrequest: <Typography variant="h6">Bad request @ {location.pathname}</Typography>,
+        noroute: <Typography variant="h6">Sijaintia {location.pathname} ei valitettavasti löydy.</Typography>,
+        unauthorized: (
+            <Typography variant="h6">
+                Sessio on mahdollisesti vanhentunut. Kirjaudu uudestaan sisään ja yritä uudelleen ole hyvä!
+            </Typography>
+        ),
+        forbidden: <Typography variant="h6">Käyttöoikeudet eivät riitä.</Typography>,
+        teapot: <Typography variant="h6">Minä olen teepannu -- siksi en keitä kahvia!</Typography>,
         internalservererror: (
             <Typography variant="h6">
-                Server encountered an unexpected condition that prevented it from fulfilling the request @{' '}
-                {location.pathname}
+                Sisäinen palvelinvirhe. Palvelin kohtasi odottamattoman tilan, mikä esti pyynnön toteuttamisen.
             </Typography>
+        ),
+        serviceunavailable: (
+            <Typography variant="h6">Huollamme paraikaa järjestelmäämme! Tule takaisin myöhemmin uudelleen.</Typography>
         ),
         else: <Typography variant="h6">Virhe sijainnissa {location.pathname}</Typography>,
     };
@@ -45,14 +66,14 @@ function ErrorBoundary() {
             <Alert severity="error">
                 <AlertTitle>Jokin meni pieleen</AlertTitle>
                 {errorTypes[errorType(error)]}
-                <Box>
-                    <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
-                        Takaisin
-                    </Button>
-                    <Button component={Link} to="/" sx={{ margin: '1em' }}>
-                        Takaisin etusivulle
-                    </Button>
-                </Box>
+                {/* <Box> */}
+                <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
+                    Takaisin
+                </Button>
+                <Button component={Link} to="/" sx={{ margin: '1em' }}>
+                    Tavarat Kiertoon etusivulle
+                </Button>
+                {/* </Box> */}
             </Alert>
         </Box>
     );
