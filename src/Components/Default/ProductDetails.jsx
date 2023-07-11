a8a9b1852c8d503bb02ac06fe34a686dd2951cd1
Martin80451 <martin.malmivirtahky@gmail.com>
2023-07-11 13:47:46 +0300

    Changed the way product infromation is displayed + added more information.

--- src/Components/Default/ProductDetails.jsx
+++ src/Components/Default/ProductDetails.jsx
@@ -5,6 +5,7 @@ import {
     Button,
     Card,
     CardActions,
+    CardActionArea,
     CardContent,
     CardMedia,
     Typography,
@@ -12,6 +13,7 @@ import {
     ImageListItem,
     Container,
     Grid,
+    Paper,
 } from '@mui/material';
 
 import AuthContext from '../../Context/AuthContext';
@@ -22,9 +24,21 @@ function ProductDetails() {
     const data = useLoaderData();
     const { id: productId } = useParams();
 
-    const { name: productName, free_description: description, date, category, barcode } = data;
+    const {
+        name: productName,
+        free_description: description,
+        date,
+        category,
+        amount,
+        measurements,
+        weight,
+        color,
+        barcode,
+    } = data;
     const [image, setImage] = useState(data.pictures[0].picture_address);
     const { auth } = useContext(AuthContext);
+
+    console.log({ ...color });
 
     return (
         <Container id="product-detail-card">
@@ -36,13 +50,15 @@ function ProductDetails() {
                     <Card>
                         <Grid container id="product-img-and-details-container">
                             <Grid item xs={6}>
-                                <CardMedia
-                                    component="img"
-                                    alt="product image"
-                                    height="460"
-                                    image={`${window.location.protocol}//${window.location.hostname}:8000/media/${image}`}
-                                />
-                                <ImageList cols={6} rowHeight={164}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="product image"
+                                        height="460"
+                                        image={`${window.location.protocol}//${window.location.hostname}:8000/media/${image}`}
+                                    />
+                                </CardActionArea>
+                                <ImageList cols={6} rowHeight={164} sx={{ pl: 2 }}>
                                     {data.pictures.map((pic) => (
                                         <ImageListItem
                                             key={pic.picture_address}
@@ -60,44 +76,78 @@ function ProductDetails() {
                             </Grid>
                             <Grid item xs={6}>
                                 <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
+                                    <Typography gutterBottom variant="h5" align="center" component="div">
                                         {productName}
                                     </Typography>
-                                    {/* show id if component used in storageview or admin */}
-                                    {auth.storage || auth.admin ? (
-                                        <Typography variant="body6" color="text.secondary">
-                                            Product id: {productId}
-                                        </Typography>
-                                    ) : null}
-
-                                    <Typography variant="body1" color="text.secondary" gutterBottom>
-                                        Tuotekuvaus: {description}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Added on: {new Date(date).toLocaleDateString('fi-FI')}
-                                    </Typography>
-                                    {/* generate barcode if component used in storageview or admin */}
-                                    {auth.storage || auth.admin ? (
-                                        <Typography variant="body2" color="text.secondary">
-                                            Barcode: {barcode}
-                                        </Typography>
-                                    ) : null}
-                                    {/* miten näyttää kategoriat, buttoneina? */}
-                                    <Typography variant="body2" color="text.secondary">
-                                        Kategoriat:
-                                    </Typography>
-                                    <Button variant="contained" size="small" disabled>
-                                        {/* to be implemented when backend is ready */}
-                                        {category}
-                                    </Button>
-
-                                    <CardActions>
-                                        <AddToCartButton
-                                            size="medium"
-                                            id={Number(productId)}
-                                            productName={productName}
-                                        />
-                                    </CardActions>
+                                    <Paper
+                                        variant="outlined"
+                                        component={Grid}
+                                        container
+                                        direction="column"
+                                        gap={2}
+                                        sx={{ p: 2 }}
+                                    >
+                                        <Grid item>
+                                            <Typography variant="body1" color="text.secondary" gutterBottom>
+                                                Tuotekuvaus: {description}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid container direction="row" gap={5}>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Lisätty: {new Date(date).toLocaleDateString('fi-FI')}
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Määrä: {amount}
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid container direction="row" gap={5}>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Paino: {weight}
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Mitat: {measurements}
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid container direction="row" gap={5}>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Värit: {color.join(', ')}
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Kategoriat:
+                                                </Typography>
+                                                <Button
+                                                    variant="contained"
+                                                    size="small"
+                                                    sx={{ width: '4rem' }}
+                                                    disabled
+                                                >
+                                                    {/* to be implemented when backend is ready */}
+                                                    {category}
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                        {/* miten näyttää kategoriat, buttoneina? */}
+                                    </Paper>
+                                    <Grid container justifyContent="center" sx={{ mt: 5 }}>
+                                        <CardActions>
+                                            <AddToCartButton
+                                                size="large"
+                                                id={Number(productId)}
+                                                productName={productName}
+                                            />
+                                        </CardActions>
+                                    </Grid>
                                 </CardContent>
                             </Grid>
                         </Grid>
@@ -109,3 +159,17 @@ function ProductDetails() {
 }
 
 export default ProductDetails;
+
+// {/* show id if component used in storageview or admin */}
+// {auth.storage || auth.admin ? (
+//     <Typography variant="body6" color="text.secondary">
+//         Product id: {productId}
+//     </Typography>
+// ) : null}
+
+// {/* generate barcode if component used in storageview or admin */}
+// {auth.storage || auth.admin ? (
+//     <Typography variant="body2" color="text.secondary">
+//         Barcode: {barcode}
+//     </Typography>
+// ) : null}
