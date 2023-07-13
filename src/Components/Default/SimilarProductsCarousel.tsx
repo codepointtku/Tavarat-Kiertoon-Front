fb9fdd7b46e88bb9a67a9691c84b6e459695087f
Martin80451 <martin.malmivirtahky@gmail.com>
2023-07-13 08:11:57 +0300

    Created carousel component for product details.

---
+++ src/Components/Default/SimilarProductsCarousel.tsx
@@ -0,0 +1,8 @@
+import { Paper, Card } from '@mui/material';
+import { Carousel } from 'react-responsive-carousel';
+
+function SimilarProductsCarousel() {
+    return <Carousel>{/* <Paper>a</Paper> */}</Carousel>;
+}
+
+export default SimilarProductsCarousel;
