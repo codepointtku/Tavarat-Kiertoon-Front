ece79838b7182ad2a4de59b2c6a729709102764f
Martin80451 <martin.malmivirtahky@gmail.com>
2023-07-14 12:41:23 +0300

    Changed amount of similar products per page to 3.

--- src/Components/Default/SimilarProductsCarousel.tsx
+++ src/Components/Default/SimilarProductsCarousel.tsx
@@ -9,23 +9,24 @@ interface Props {
 
 function SimilarProductsCarousel({ similarProducts }: Props) {
     const carouselPageContents = [];
-    const pageCount = similarProducts.results.length / 4;
+    const pageCount = similarProducts.results.length / 3;
     var a = 0;
 
     for (let i = 0; i < pageCount; i++) {
-        a += 4;
-        carouselPageContents.push(similarProducts.results.slice(a - 4, a));
+        a += 3;
+        carouselPageContents.push(similarProducts.results.slice(a - 3, a));
     }
 
     return (
         <Paper variant="outlined" sx={{ p: 2, mb: 5, borderColor: 'primary.light' }}>
             <Carousel
                 autoPlay={false}
+                cycleNavigation={false}
                 indicatorIconButtonProps={{ style: { color: '#bfe6f6' } }}
                 activeIndicatorIconButtonProps={{ style: { color: '#009bd8' } }}
             >
                 {carouselPageContents.map((productCards) => (
-                    <Grid container direction="row" gap={5} sx={{ pl: 5 }}>
+                    <Grid container direction="row" justifyContent="center" gap={5}>
                         {productCards.map((product) => (
                             <SimilarProductCard product={product as SimilarProduct['product']} />
                         ))}
