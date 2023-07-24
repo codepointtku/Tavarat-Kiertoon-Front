d08b23dbecbc73d75bb77edac42d884b1ceac02b
J. Waananen <j.waananen@protonmail.com>
2023-07-24 08:59:02 +0300

    modal template

---
+++ src/Components/BulletinModal.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+
+import { Backdrop, Box, Modal, Fade, Typography, Stack } from '@mui/material/';
+
+interface Props {
+    title: string;
+    content?: string;
+    footer?: string | JSX.Element;
+}
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '0.1rem solid #009bd8',
+    borderRadius: '1rem',
+    boxShadow: 24,
+    p: 4,
+};
+
+function BulletinModal({ title, content, footer }: Props) {
+    const [bulletinModalOpened, setBulletinModalOpened] = React.useState(true);
+    // const handleOpen = () => setMessageModalOpened(true);
+    const handleClose = () => setBulletinModalOpened(false);
+
+    return (
+        <div>
+            <Modal
+                aria-labelledby="bulletin-modal-title"
+                aria-describedby="bulletin-modal-description"
+                open={bulletinModalOpened}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={bulletinModalOpened} easing={'2000'}>
+                    <Box sx={style}>
+                        <Stack>
+                            <Typography id="bulletin-modal-title" variant="h6" component="h2" textAlign="center">
+                                {title}
+                            </Typography>
+                            <Typography id="bulletin-modal-content" sx={{ mt: 2 }} textAlign="center">
+                                {content}
+                            </Typography>
+                            {footer}
+                        </Stack>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
+
+export default BulletinModal;
