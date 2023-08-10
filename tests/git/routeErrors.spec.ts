d6ba1e9fe5ab175ee1870c06f1e5cf091dc6be8c
J. Waananen <j.waananen@protonmail.com>
2023-08-10 13:28:12 +0300

    route test for order delete

--- tests/git/routeErrors.spec.ts
+++ tests/git/routeErrors.spec.ts
@@ -232,14 +232,21 @@ test.describe('admin orderlist', () => {
 
 test.describe('admin order view', () => {
     test('no errors', async ({ page }) => {
-        await page.goto('/admin/tilaukset/13');
+        await page.goto('/admin/tilaukset/16');
         await expect(page.getByText('Tilausnumeroa ei löytynyt.')).not.toBeVisible();
     });
 });
 
 test.describe('admin order edit', () => {
     test('no errors', async ({ page }) => {
-        await page.goto('/admin/tilaukset/13');
+        await page.goto('/admin/tilaukset/16');
+        await expect(page.getByText('Tilausnumeroa ei löytynyt.')).not.toBeVisible();
+    });
+});
+
+test.describe('admin order delete', () => {
+    test('no errors', async ({ page }) => {
+        await page.goto('/admin/tilaukset/16/poista');
         await expect(page.getByText('Tilausnumeroa ei löytynyt.')).not.toBeVisible();
     });
 });
