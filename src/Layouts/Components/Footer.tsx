fb16345eec9f6dd0d0968313e8aa06f37628c72f
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-08-21 10:36:52 +0300

    improved footer

--- src/Layouts/Components/Footer.tsx
+++ src/Layouts/Components/Footer.tsx
@@ -1,21 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useRouteLoaderData } from 'react-router-dom';
 
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import GitHubIcon from '@mui/icons-material/GitHub';
 import { Box, Grid, Link as MuiLink, Typography } from '@mui/material';
+import { type rootLoader } from '../../Router/loaders';
 
 function Footer() {
+    const { contacts } = useRouteLoaderData('root') as Awaited<ReturnType<typeof rootLoader>>;
+
     return (
         <Box id="footer-container" sx={{ backgroundColor: 'primary.main' }}>
             <Grid container justifyContent="space-around" alignItems="center" flexGrow="1" padding="1rem">
                 <Grid item xs={12} md={6} lg={4} color="primary.contrastText" flexDirection="column" textAlign="center">
-                    <Typography variant="subtitle2" gutterBottom>
-                        Yhteystiedot
-                    </Typography>
-                    <MuiLink href="https://github.com/codepointtku/Tavarat-Kiertoon-Front" color="#663900">
-                        <GitHubIcon />
-                    </MuiLink>
+                    © 2023 Turun Kaupunki
                 </Grid>
-
                 <Grid item xs={12} md={6} lg={4} color="primary.contrastText" flexDirection="column" textAlign="center">
                     <Typography variant="subtitle2" color="primary.contrastText">
                         Tietoa
@@ -30,9 +27,23 @@ function Footer() {
                         Taustaa
                     </MuiLink>
                 </Grid>
-
                 <Grid item xs={12} md={6} lg={4} color="primary.contrastText" flexDirection="column" textAlign="center">
-                    xxxx
+                    <Typography variant="subtitle2" gutterBottom>
+                        Yhteystiedot
+                    </Typography>
+                    <MuiLink component={Link} to="/otayhteytta" color="white">
+                        Ota Yhteyttä - lomake
+                    </MuiLink>
+                    {contacts[0] && (
+                        <>
+                            <Typography variant="body2" gutterBottom>
+                                {contacts[0]?.address}
+                            </Typography>
+                            <Typography variant="body2" gutterBottom>
+                                {contacts[0]?.phone_number}
+                            </Typography>
+                        </>
+                    )}
                 </Grid>
             </Grid>
         </Box>
