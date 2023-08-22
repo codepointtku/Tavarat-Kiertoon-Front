1b421778c19c247a80b743ec05b1f860e9afb396
J. Waananen <j.waananen@protonmail.com>
2023-08-22 08:46:14 +0300

    rm unused import

--- src/Components/Admin/ProductsGrid.tsx
+++ src/Components/Admin/ProductsGrid.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { useLoaderData } from 'react-router';
 import { Link } from 'react-router-dom';
 
-import { Stack, Button, Link as MuiLink } from '@mui/material';
+import { Stack, Button } from '@mui/material';
 
 import {
     DataGrid,
