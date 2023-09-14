2c53a372109d47b50138871d561cd547c3ee60ce
J. Waananen <j.waananen@protonmail.com>
2023-09-14 11:46:30 +0300

    i think passwordChange component is okay now

--- src/Components/Default/ResetPassword/PasswordChange.tsx
+++ src/Components/Default/ResetPassword/PasswordChange.tsx
@@ -1,58 +1,85 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useSubmit, useActionData, Form } from 'react-router-dom';
+import { Form, useSubmit, Link } from 'react-router-dom';
 
-import { Typography, Box, Container, TextField, Button, Alert, AlertTitle, Stack } from '@mui/material';
+import { Typography, Container, TextField, Button, Stack, Link as MuiLink } from '@mui/material';
 import KeyIcon from '@mui/icons-material/Key';
 
 import HeroHeader from '../../HeroHeader';
 import HeroText from '../../HeroText';
 
-import type { resetEmailAction } from '../../../Router/actions';
-import { Link } from 'react-router-dom';
+const SuperLink = MuiLink as typeof MuiLink | typeof Link;
+
+function MsgFooter() {
+    return (
+        <Typography>
+            <SuperLink component={Link} to={'/tili'}>
+                Palaa takaisin tilisivulle
+            </SuperLink>{' '}
+            tai{' '}
+            <SuperLink component={Link} to="/">
+                siirry etusivulle
+            </SuperLink>
+        </Typography>
+    );
+}
 
 function PasswordChange() {
-    const responseStatus = useActionData() as Awaited<ReturnType<typeof resetEmailAction>>;
+    const [linkSent, setLinkSent] = useState(false);
 
     const {
         register,
         handleSubmit,
-        formState: { errors },
-    } = useForm();
+        formState: { isValid, errors },
+    } = useForm({
+        mode: 'all',
+    });
 
     const submit = useSubmit();
 
     const onSubmit = (data: any) => {
         const { username } = data;
         submit({ username }, { method: 'post', action: '/salasananvaihto/' });
+        setLinkSent(true);
     };
 
     return (
         <>
-            <Container maxWidth="md" sx={{ border: '0.1rem solid #bfe6f6', borderRadius: '1rem', pt: 3, mb: 2 }}>
+            <Container maxWidth="md" sx={{ border: '0.1rem solid #bfe6f6', borderRadius: '1rem', p: 3, mb: 2 }}>
                 <HeroHeader Icon={<KeyIcon />} hideInAdmin />
-                <HeroText
-                    title="Salasanan vaihto"
-                    subtext2="Lähetämme syöttämääsi sähköpostiosoitteeseen linkin, josta voit suorittaa tiliin liitetyn sähköpostin vaihdon."
-                />
-                <Container maxWidth="md" sx={{ my: 3 }}>
-                    {responseStatus?.status ? (
-                        <Typography variant="h6" textAlign="center" gutterBottom>
-                            Salasanan palautuslinkki on nyt lähetetty.
-                        </Typography>
+                {!linkSent && (
+                    <HeroText
+                        title="Salasanan vaihto"
+                        subtext2="Lähetämme syöttämääsi sähköpostiosoitteeseen linkin, josta voi suorittaa tilin salasanan vaihdon."
+                    />
+                )}
+                <Container maxWidth="md">
+                    {linkSent ? (
+                        <HeroText
+                            title="Salasanan vaihtolinkki on nyt lähetetty."
+                            subtitle="Olet vielä sisäänkirjautuneena tilillesi nykyisellä salasanalla."
+                            subtext=" Seuraa sähköpostista löytyvän linkin ohjeita tilin salasanan vaihtamiseksi."
+                            footer={<MsgFooter />}
+                        />
                     ) : (
-                        <Stack component={Form} onSubmit={handleSubmit(onSubmit)} alignItems="center" spacing={3}>
+                        <Stack component={Form} onSubmit={handleSubmit(onSubmit)} alignItems="center" spacing={1}>
                             <TextField
                                 label="Sähköpostiosoite"
-                                {...register('username', { required: true, minLength: 4, maxLength: 60 })}
+                                {...register('username', {
+                                    required: { value: true, message: 'Syötä sähköpostiosoite' },
+                                    //this pattern will match a string containing at least:
+                                    // `@` symbol with any character before and after it,
+                                    // + a dot `.` and any character after it
+                                    pattern: { value: /.+@.+\..+/, message: 'Syötteen on oltava sähköpostiosoite' },
+                                })}
+                                inputProps={{ required: false }}
+                                required
+                                error={!!errors.username}
+                                helperText={errors.username?.message?.toString() || ' '}
+                                color={isValid ? 'success' : 'primary'}
                                 fullWidth
                             />
-                            {errors.username && (
-                                <Alert severity="error" sx={{ mt: 1, maxWidth: 300 }}>
-                                    <AlertTitle>Virheellinen syöte</AlertTitle>
-                                </Alert>
-                            )}
-
-                            <Button id="submit-btn" type="submit" sx={{ mt: 2, fontWeight: 'fontWeightMediumBold' }}>
+                            <Button id="submit-btn" type="submit" sx={{ mt: 2 }}>
                                 Lähetä salasanan palautuslinkki
                             </Button>
                         </Stack>
