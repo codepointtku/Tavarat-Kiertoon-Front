9644963b9980a97d8fabd149a676da8fa1d4a720
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-09-19 08:41:30 +0300

    pagination for orders

--- src/Components/Storage/DataTable.jsx
+++ src/Components/Storage/DataTable.jsx
@@ -1,52 +1,58 @@
 import { useLoaderData, useNavigate } from 'react-router-dom';
 
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 import StyledTableCell from '../StyledTableCell';
 import SortByMenu from '../SortByMenu';
+import Pagination from '../Pagination';
 
 function OrderListTable() {
-    const { results: orders } = useLoaderData();
+    const { results: orders, count } = useLoaderData();
     const navigate = useNavigate();
 
     return (
-        <TableContainer sx={{ paddingY: '2rem' }}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>
-                            Tilaus (ID) <SortByMenu />
-                        </StyledTableCell>
-                        <StyledTableCell align="right">Status</StyledTableCell>
-                        <StyledTableCell align="right">Toimitusosoite</StyledTableCell>
-                        <StyledTableCell align="right">Tilaaja</StyledTableCell>
-                        <StyledTableCell align="right">Yhteystiedot</StyledTableCell>
-                        <StyledTableCell align="right">Päivämäärä</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {orders?.map((row) => (
-                        <TableRow
-                            key={row.id}
-                            style={{ cursor: 'pointer' }}
-                            hover
-                            onClick={() => navigate(`/varasto/tilaukset/${row.id}`)}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.id}
-                            </TableCell>
-                            <TableCell align="right">{row.status}</TableCell>
-                            <TableCell align="right">{row.delivery_address}</TableCell>
-                            <TableCell align="right">{row.user}</TableCell>
-                            <TableCell align="right">{row.contact}</TableCell>
-                            <TableCell align="right">
-                                {row.creation_date ? new Date(row.creation_date).toLocaleDateString('fi-FI') : '-'}
-                            </TableCell>
+        <>
+            <TableContainer sx={{ paddingY: '2rem' }}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>
+                                Tilaus (ID) <SortByMenu />
+                            </StyledTableCell>
+                            <StyledTableCell align="right">Status</StyledTableCell>
+                            <StyledTableCell align="right">Toimitusosoite</StyledTableCell>
+                            <StyledTableCell align="right">Tilaaja</StyledTableCell>
+                            <StyledTableCell align="right">Yhteystiedot</StyledTableCell>
+                            <StyledTableCell align="right">Päivämäärä</StyledTableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {orders?.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                style={{ cursor: 'pointer' }}
+                                hover
+                                onClick={() => navigate(`/varasto/tilaukset/${row.id}`)}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.id}
+                                </TableCell>
+                                <TableCell align="right">{row.status}</TableCell>
+                                <TableCell align="right">{row.delivery_address}</TableCell>
+                                <TableCell align="right">{row.user}</TableCell>
+                                <TableCell align="right">{row.contact}</TableCell>
+                                <TableCell align="right">
+                                    {row.creation_date ? new Date(row.creation_date).toLocaleDateString('fi-FI') : '-'}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Pagination count={count} itemsText="Tilauksia" />
+            </Box>
+        </>
     );
 }
 
