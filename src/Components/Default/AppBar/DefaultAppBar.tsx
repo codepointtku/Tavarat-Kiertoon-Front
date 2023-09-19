cacb2d9fe80b3a6de9142c63759fe8a3a13acfd7
J. Waananen <j.waananen@protonmail.com>
2023-09-19 12:17:11 +0300

    rm animation from cart badge

--- src/Components/Default/AppBar/DefaultAppBar.tsx
+++ src/Components/Default/AppBar/DefaultAppBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect, type ReactNode } from 'react';
+import { useState, useContext, /* useEffect, */ type ReactNode } from 'react';
 import { useLoaderData, useNavigate, useLocation, useFetcher } from 'react-router-dom';
 
 import {
