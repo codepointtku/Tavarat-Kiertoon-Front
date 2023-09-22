40a1d934d4f7e35c8510e267ec65f8b2def2eed5
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-09-22 13:15:40 +0300

    Return of LoginPage

---
+++ src/Components/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from './LoginForm';
+import NavLinksForStaff from './NavLinksForStaff';
+
+function LoginPage() {
+    return (
+        <>
+            <LoginForm />
+            <NavLinksForStaff />
+        </>
+    );
+}
+
+export default LoginPage;
