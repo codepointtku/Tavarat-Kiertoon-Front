81a7a69255c8d788692efca18af54aed8a019d4d
J. Waananen <j.waananen@protonmail.com>
2023-10-04 11:27:44 +0300

    data flow

--- src/Components/Admin/CategoriesManage.tsx
+++ src/Components/Admin/CategoriesManage.tsx
@@ -1,4 +1,12 @@
+import { useLoaderData } from 'react-router-dom';
+import type { categoriesManageLoader } from '../../Router/loaders';
+
 function CategoriesManage() {
+    const { categories, categoryTree } = useLoaderData() as Awaited<ReturnType<typeof categoriesManageLoader>>;
+
+    console.log('cats', categories);
+    console.log('tree', categoryTree);
+
     return 'jeejee';
 }
 
