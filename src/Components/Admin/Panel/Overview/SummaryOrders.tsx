99c871ba6b1378722e50e79f062e0ca750d32ac6
J. Waananen <j.waananen@protonmail.com>
2023-11-08 11:31:52 +0200

    now just gotta see how to set order.day to correct it's spot on the graph

--- src/Components/Admin/Panel/Overview/SummaryOrders.tsx
+++ src/Components/Admin/Panel/Overview/SummaryOrders.tsx
@@ -18,29 +18,8 @@ import {
 import { Line } from 'react-chartjs-2';
 
 import type { gigaLoader } from '../../../../Router/loaders';
-import { getDay } from 'date-fns';
-
-// import type { ChartData, ChartOptions } from 'chart.js';
-
-// interface LineProps {
-//     options: ChartOptions<'line'>;
-//     data: ChartData<'line'>;
-// }
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
-
-// const options: LineProps = {
-//     plugins: {
-//         legend: {
-//             position: 'top',
-//             // display: false,
-//         },
-//         title: {
-//             display: true,
-//             text: titletext,
-//         },
-//     },
-// };
 
 async function getDayz() {
     const now = new Date();
@@ -57,6 +36,8 @@ async function getDayz() {
 function LineChartti() {
     const { ordersList } = useLoaderData() as Awaited<ReturnType<typeof gigaLoader>>;
 
+    // console.log(ordersList.results);
+
     const [jee, setJee] = useState<string[]>([]);
 
     useEffect(() => {
@@ -68,30 +49,56 @@ function LineChartti() {
         fetchDayz();
     }, []);
 
-    const creationDatetMillisekunteina = ordersList.results?.map(
-        (order) => new Date(order.creation_date).getTime() /*.toLocaleDateString('fi-FI')*/
+    // const currentMonth = (new Date().getMonth() + 1).toString();
+    const currentMonth = new Date().toISOString().slice(0, 7);
+    // console.log(currentMonth);
+
+    // const creationDatetMillisekunteina = ordersList.results?.map((order) => new Date(order.creation_date).getTime());
+    const ordersCreationDates = ordersList.results?.map((order) =>
+        new Date(order.creation_date).toISOString().slice(0, 10)
     );
 
-    const data = {
-        jee,
-        datasets: [
-            {
-                fill: true,
-                label: '2023',
-                data: creationDatetMillisekunteina?.map((paiva) => paiva),
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            },
-        ],
-    };
+    // console.log(ordersCreationDates);
 
-    return <Line /* options={options} */ data={data} />;
+    // const creationDatet = ordersList.results?.map((order) => order.creation_date.slice(0, 10));
+    // console.log(creationDatet);
+
+    const currentMonthOrders = ordersCreationDates?.filter((order) => order.includes(currentMonth));
+    // console.log('curry orders:', currentMonthOrders);
+
+    const curryOrdersAsTime = currentMonthOrders?.map((orderDate) => new Date(orderDate).getTime());
+    // console.log('curryOrdersAsTime:', curryOrdersAsTime);
+
+    if (jee.length === 0) {
+        return null;
+    }
+
+    // console.log('jee ennen returnia', jee);
+    // console.log('data ennen returnia', creationDatetMillisekunteina);
+
+    return (
+        <Line
+            data={{
+                labels: jee,
+                datasets: [
+                    {
+                        fill: true,
+                        label: 'Kuluvan kuukauden tilausmäärät',
+                        // data: [2, 4, 54, 6, 7, 5, 3, 2],
+                        data: curryOrdersAsTime,
+                        borderColor: 'rgb(53, 162, 235)',
+                        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    },
+                ],
+            }}
+        />
+    );
 }
 
 function SummaryOrders() {
     return (
         <Stack alignItems="center" flex={1}>
-            <Typography variant="subtitle2">Kuluvan kuukauden tilausmäärät</Typography>
+            {/* <Typography variant="subtitle2">Kuluvan kuukauden tilausmäärät</Typography> */}
             <LineChartti />
         </Stack>
     );
