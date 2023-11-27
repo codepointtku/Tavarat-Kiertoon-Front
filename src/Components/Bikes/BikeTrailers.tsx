0fe4bcda4bb257157db716dd10aeaa619ac05318
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-11-27 12:42:28 +0200

    minor UI tweaks, trailer_type is handled in backend now

--- src/Components/Bikes/BikeTrailers.tsx
+++ src/Components/Bikes/BikeTrailers.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, useSubmit, useLoaderData, useActionData, } from 'react-router-dom';
+import { Form, useSubmit, useLoaderData, useActionData } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import StyledTableCell from '../StyledTableCell';
 
@@ -16,6 +16,7 @@ import {
     Button,
     Container,
     TextField,
+    MenuItem,
 } from '@mui/material';
 
 import DeleteBikeTrailerModal from './DeleteBikeTrailerModal';
@@ -31,49 +32,56 @@ export default function BikeTrailers() {
     const submit = useSubmit();
     const onSubmit = (data: any) => {
         submit(data, { method: 'post', action: '/pyorat/pyoravarasto/perakarryt' });
-        reset({ register_number: '', trailer_type: 1 });
+        reset({ register_number: '' });
     };
 
-    const { handleSubmit, register, watch, reset, formState: { isDirty, isValid, errors, isSubmitting }, } = useForm({
+    const {
+        handleSubmit,
+        register,
+        watch,
+        reset,
+        formState: { isDirty, isValid, errors, isSubmitting },
+    } = useForm({
         defaultValues: {
             register_number: '',
-            trailer_type: 1,
         },
     });
 
-    console.log(deleteModalItem)
+    const handleRegisterNumber = (e) => {
+        const reg = new RegExp("[A-Z]")
+    }
 
-    console.log(data)
+    console.log(deleteModalItem);
+
+    console.log(data);
     return (
         <Box width="100%">
             <Typography variant="h3" align="center" color="primary.main" width="100%" sx={{ margin: '0 0 1rem 0' }}>
                 Peräkärryt
             </Typography>
             <Container maxWidth="xl" component={Paper}>
-                <Box component={Form} onSubmit={handleSubmit(onSubmit)}>
+                <Box
+                    component={Form}
+                    sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <TextField
-                            id="register-number"
-                            type="text"
-                            label="Rekisterinumero"
-                            error={!!errors.register_number}
-                            {...register('register_number', {
+                        id="register-number"
+                        type="text"
+                        label="Rekisterinumero"
+                        error={!!errors.register_number}
+                        {...register('register_number', {
                             required: {
                                 value: true,
                                 message: 'Rekisterinumero puuttuu',
                             },
-                            }
-                            )}
-                    >
-                    </TextField>
-                    <Button
-                        type="submit"
-                        disabled={!isDirty || !isValid || isSubmitting}
-                    >
+                        })}
+                    ></TextField>
+                    <Button type="submit" sx={{ marginLeft: '1rem' }} disabled={!isDirty || !isValid || isSubmitting}>
                         Lisää peräkärry
                     </Button>
                 </Box>
-                <TableContainer sx={{ padding: '2rem' }} component={Form}
-                        onSubmit={handleSubmit(onSubmit)}>
+                <TableContainer sx={{ padding: '2rem' }} component={Form} onSubmit={handleSubmit(onSubmit)}>
                     <Box width="20%"></Box>
                     <Table sx={{ minWidth: 700 }} aria-label="customized table">
                         <TableHead>
@@ -84,15 +92,12 @@ export default function BikeTrailers() {
                                 <StyledTableCell align="left" width="70%">
                                     Rekisterinumero
                                 </StyledTableCell>
-                                <StyledTableCell align="left" width="10%">
-                                </StyledTableCell>
+                                <StyledTableCell align="left" width="10%"></StyledTableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             {data?.map((trailer) => (
-                                <TableRow
-                                    key={trailer.id}
-                                >
+                                <TableRow key={trailer.id}>
                                     <TableCell align="left">{trailer.id}</TableCell>
                                     <TableCell align="left">{trailer.register_number}</TableCell>
                                     <TableCell align="right">
@@ -100,7 +105,9 @@ export default function BikeTrailers() {
                                             id="delete-button"
                                             type="button"
                                             color="error"
-                                            onClick={() => {setDeleteModalItem(trailer), setRenderDeleteBikeTrailerModal(true)}}
+                                            onClick={() => {
+                                                setDeleteModalItem(trailer), setRenderDeleteBikeTrailerModal(true);
+                                            }}
                                         >
                                             Poista
                                         </Button>
