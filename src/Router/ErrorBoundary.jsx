9f729df625501b9991f69f5886ae012afb512ba7
codepointpetteri <102511124+codepointpetteri@users.noreply.github.com>
2025-04-08 13:06:45 +0300

    added 403 forbidden to give unauthorized error

--- src/Router/ErrorBoundary.jsx
+++ src/Router/ErrorBoundary.jsx
@@ -12,7 +12,12 @@ function ErrorBoundary() {
 
     const errorType = (err) => {
         console.log(err);
-        if (err?.status === 401 || err?.response?.status === 401) {
+        if (
+            err?.status === 401 ||
+            err?.response?.status === 403 ||
+            err?.status === 403 ||
+            err?.response?.status === 403
+        ) {
             console.log('unauthorized in errorboundary');
             return 'unauthorized';
         }
@@ -27,36 +32,60 @@ function ErrorBoundary() {
 
     return (
         <Box minHeight={320}>
-            <Alert severity="warning">
-                <AlertTitle>Jokin meni pieleen</AlertTitle>
+            {
                 {
-                    {
-                        // TODO: navigoi pyöräpuolen etusivulle, riippuen mistä tultiin?
-                        //Vaihtoehtoisesti: poista, HasRole-komponentti hoitaa uudelleenohjauksen eri osioissa?
-                        unauthorized: <Navigate to="/kirjaudu" />,
-                        axios: (
+                    // TODO: navigoi pyöräpuolen etusivulle, riippuen mistä tultiin?
+                    //Vaihtoehtoisesti: poista, HasRole-komponentti hoitaa uudelleenohjauksen eri osioissa?
+                    unauthorized: <Navigate to="/kirjaudu" />,
+                    axios: (
+                        <Alert severity="warning">
+                            <AlertTitle>Jokin meni pieleen</AlertTitle>
                             <Typography variant="h6">
                                 Yhteysongelma sijainnissa {location.pathname} , (Axios error).
                             </Typography>
-                        ),
+                            <Box>
+                                <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
+                                    Takaisin
+                                </Button>
+                                <Button component={Link} reloadDocument to="/" sx={{ margin: '1em' }}>
+                                    Takaisin etusivulle
+                                </Button>
+                            </Box>
+                        </Alert>
+                    ),
 
-                        noroute: (
+                    noroute: (
+                        <Alert severity="warning">
+                            <AlertTitle>Jokin meni pieleen</AlertTitle>
                             <Typography variant="h6">
                                 Etsimääsi sijaintia {location.pathname} ei valitettavasti löydy.
                             </Typography>
-                        ),
-                        else: <Typography variant="h6">Virhe sijainnissa {location.pathname}.</Typography>,
-                    }[errorType(error)]
-                }
-                <Box>
-                    <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
-                        Takaisin
-                    </Button>
-                    <Button component={Link} reloadDocument to="/" sx={{ margin: '1em' }}>
-                        Takaisin etusivulle
-                    </Button>
-                </Box>
-            </Alert>
+                            <Box>
+                                <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
+                                    Takaisin
+                                </Button>
+                                <Button component={Link} reloadDocument to="/" sx={{ margin: '1em' }}>
+                                    Takaisin etusivulle
+                                </Button>
+                            </Box>
+                        </Alert>
+                    ),
+                    else: (
+                        <Alert severity="warning">
+                            <AlertTitle>Jokin meni pieleen</AlertTitle>
+                            <Typography variant="h6">Virhe sijainnissa {location.pathname}.</Typography>
+                            <Box>
+                                <Button onClick={handleGoBack} sx={{ margin: '1em' }}>
+                                    Takaisin
+                                </Button>
+                                <Button component={Link} reloadDocument to="/" sx={{ margin: '1em' }}>
+                                    Takaisin etusivulle
+                                </Button>
+                            </Box>
+                        </Alert>
+                    ),
+                }[errorType(error)]
+            }
         </Box>
     );
 }
